af5ecda9fed3f774131935d53036cb3a6d17b2d0
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2023-09-16 15:27:17 +0200

    disabled analytics.

--- con-organizer/lib/firebase.ts
+++ con-organizer/lib/firebase.ts
@@ -2,7 +2,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
@@ -23,7 +23,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 export const auth = getAuth();
 
