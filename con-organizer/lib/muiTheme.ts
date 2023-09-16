1db96f5116d22924d37cac509e21aeb72d5e16f0
Christer M.L. Bendixen <gitstuff@fortelle.com>
2023-09-16 16:15:05 +0200

    flikket på fargene

--- con-organizer/lib/muiTheme.ts
+++ con-organizer/lib/muiTheme.ts
@@ -5,16 +5,21 @@ import { createTheme, ThemeOptions } from '@mui/material';
 const muiLightTheme: ThemeOptions = {
         palette: {
             primary: {
-                light: '#fff',
-                main: '#222',
+                light: '#a1887f',
+                main: '#3e2723',
                 dark: '#000',
                 contrastText: '#fff',
             },
             secondary: {
-                light: '#fff',
-                main: '#f44336',
+                light: '#ffd54f',
+                main: '#ff8f00',
                 dark: '#000',
                 contrastText: '#000',
+            },
+        },
+        typography: {
+            h6: {
+                fontWeight: 'bold',
             },
         },
     }
