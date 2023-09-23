e939853a7f5f90364917b21b11bb58fee7891a6e
Christer M.L. Bendixen <gitstuff@fortelle.com>
2023-09-23 10:47:18 +0200

    stailet EventCardBoundary og lagt til (og kommentert ut) test i EventCard

--- con-organizer/components/ErrorBoundaries/EventCardBoundary.tsx
+++ con-organizer/components/ErrorBoundaries/EventCardBoundary.tsx
@@ -1,16 +1,31 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Alert, Button, Card, Typography } from '@mui/material';
 type Props = {
     error: Error;
     resetErrorBoundary: () => void;
 };
 const EventCardBoundary = ({ error, resetErrorBoundary }: Props) => {
     return (
-        <Box sx={{ margin: '0 auto', backgroundColor: 'primary.light' }}>
-            <Typography variant="h6">Det har skjedd en feil: {error.message}</Typography>
+        <Card
+            sx={{
+                backgroundColor: 'primary.light',
+                width: { xs: '100vw', md: '500px' },
+                minHeight: '10em',
+                display: 'grid',
+                p: '1em',
+                gridTemplateRows: '1fr auto 1fr auto 1fr',
+            }}
+        >
+            <div></div>
+            <Alert severity="error" sx={{ maxHeight: '11em', overflow: 'scroll' }}>
+                <Typography variant="h6">Det har skjedd en feil:</Typography>
+                <Typography variant="body1">{error.message}</Typography>
+            </Alert>
+            <div></div>
             <Button variant="contained" sx={{ backgroundColor: 'primary.main' }} onClick={resetErrorBoundary}>
                 Pr&oslash;v igjen
             </Button>
-        </Box>
+            <div></div>
+        </Card>
     );
 };
 
--- con-organizer/components/EventCard.tsx
+++ con-organizer/components/EventCard.tsx
@@ -9,14 +9,18 @@ type Props = {
 };
 
 const EventCard = ({ conEvent }: Props) => {
+    // if (conEvent.id === '16ezgmkATFy0afYRw4d7') {
+    //     throw new Error(
+    //         'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. lorem dolor sit am et. Lorem ipsum dolor sit am et. lorem dolor sit am et. Lorem ipsum dolor, lorem ipsum dolor sit am et. Lorem ipsum'
+    //     );
+    // }
     return (
         <Card
             key={conEvent.id}
             component={Link}
             href={`/event/${conEvent.id}` as Route}
             sx={{
-                width: { xs: '100vw', md: '1080px' },
-                maxWidth: '500px',
+                width: { xs: '100vw', md: '500px' },
                 cursor: 'pointer',
                 opacity: conEvent?.published === false ? '50%' : '',
                 textDecoration: 'none',
