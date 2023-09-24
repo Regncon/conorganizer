2154aea402d6f496381e2b4f60e46b3168177941
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2023-09-24 09:45:24 +0200

    Added button to logo. Added call to action links for enrollment.

--- con-organizer/components/EventUi.tsx
+++ con-organizer/components/EventUi.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Box } from '@mui/material';
+import { Box, Dialog, Link } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Divider from '@mui/material/Divider';
@@ -19,6 +19,7 @@ import { ConEvent } from '@/models/types';
 import { useAuth } from './AuthProvider';
 import EnrollmentSelector from './EnrollmentSelector';
 import EventHeader from './EventHeader';
+import Login from './Login';
 
 type Props = {
     conEvent: ConEvent | undefined;
@@ -29,6 +30,7 @@ const EventUi = ({ conEvent }: Props) => {
     const { enrollments: enrollment } = useSingleEnrollment(conEvent?.id || '', user?.uid || '');
     const [errorMessage, setErrorMessage] = useState<string>();
     const [enrollmentChoice, setEnrollmentChoice] = useState<EnrollmentChoice>(EnrollmentChoice.NotInterested);
+    const [openLogin, setOpenLogin] = useState(false);
 
     const [description, setDescription] = useState('');
     useEffect(() => {
@@ -73,81 +75,103 @@ const EventUi = ({ conEvent }: Props) => {
             setErrorMessage(error.message);
         }
     }
-    // throw new Error(
-    //     'lorem Ipsum error in conAuthor authorization dialog box - invalid Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea quia in blanditiis mollitia exercitationem, asperiores nam quidem commodi nulla illum laborum, distinctio magnam debitis vitae rerum, maiores maxime sapiente! Quia! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea quia in blanditiis mollitia exercitationem, asperiores nam quidem commodi nulla illum laborum, distinctio magnam debitis vitae rerum, maiores maxime sapiente! Quia!'
-    // );
-    return (
-        <Card>
-            <EventHeader conEvent={conEvent} />
-            <Divider />
-            <Box className="p-4" sx={{ minHeight: '7rem', display: 'grid', gap: '.5rem' }}>
-                {parse(description || '')}
-                <span>
-                    <em>Tidspunkt: {conEvent?.pool}</em>
-                </span>
-            </Box>
 
-            <Divider />
-            <CardContent>
-                <FormControl>
-                    <FormLabel id="demo-row-radio-buttons-group-label">
-                        <Typography variant="h6">
-                            Påmelding
-                            {user ? '' : ' (Kjøp bilett og logg inn for å melde deg på)'}
-                        </Typography>
-                    </FormLabel>
-                    <RadioGroup
-                        row
-                        aria-labelledby="demo-row-radio-buttons-group-label"
-                        name="row-radio-buttons-group"
-                        defaultValue={EnrollmentChoice.NotInterested}
-                        value={enrollmentChoice}
-                        sx={{
-                            display: 'grid',
-                            width: '100vw',
-                            maxWidth: '1080px',
-                            padding: '.2em',
-                            gridAutoFlow: 'column',
-                            gridAutoColumns: '1fr',
-                            placeContent: 'center',
-                        }}
-                        onChange={(e) => {
-                            handleEnrollmentChoiceChange(e);
-                        }}
-                    >
-                        <EnrollmentSelector
-                            sx={{ display: 'grid', textAlign: 'center', p: '.4em' }}
-                            value={EnrollmentChoice.NotInterested}
-                            disabled={!user}
-                            control={<Radio size="small" />}
-                            label="Ikke interessert"
-                        />
-                        <EnrollmentSelector
-                            value={EnrollmentChoice.IfIHaveTo}
-                            disabled={!user}
-                            sx={{ display: 'grid', backgroundColor: '#00000055', textAlign: 'center', p: '.4em' }}
-                            control={<Radio size="small" />}
-                            label="Hvis jeg må"
-                        />
-                        <EnrollmentSelector
-                            value={EnrollmentChoice.Interested}
-                            disabled={!user}
-                            sx={{ display: 'grid', backgroundColor: '#000000aa', textAlign: 'center', p: '.4em' }}
-                            control={<Radio size="small" />}
-                            label="Har lyst"
-                        />
-                        <EnrollmentSelector
-                            value={EnrollmentChoice.VeryInterested}
-                            disabled={!user}
-                            control={<Radio size="small" />}
-                            label="Har veldig lyst"
-                            sx={{ display: 'grid', backgroundColor: '#000000ff', textAlign: 'center', p: '.4em' }}
-                        />
-                    </RadioGroup>
-                </FormControl>
-            </CardContent>
-            <Divider />
-        </Card>
+    return (
+        <>
+            <Card>
+                <EventHeader conEvent={conEvent} />
+                <Divider />
+                <Box className="p-4" sx={{ minHeight: '7rem', display: 'grid', gap: '.5rem' }}>
+                    {parse(description || '')}
+                    <span>
+                        <em>Tidspunkt: {conEvent?.pool}</em>
+                    </span>
+                </Box>
+
+                <Divider />
+                <CardContent>
+                    <FormControl>
+                        <FormLabel id="demo-row-radio-buttons-group-label">
+                            <Typography variant="h6">
+                                Påmelding
+                                {user ? (
+                                    ''
+                                ) : (
+                                    <>
+                                        :{' '}
+                                        <Link href="https://www.regncon.no/kjop-billett-til-regncon-xxxi/">
+                                            Kjøp bilett
+                                        </Link>
+                                        <span> og </span>
+                                        <Link
+                                            component="button"
+                                            onClick={() => {
+                                                setOpenLogin(true);
+                                            }}
+                                        >
+                                            logg inn
+                                        </Link>
+                                        <span> for å melde deg på.</span>
+                                    </>
+                                )}
+                            </Typography>
+                        </FormLabel>
+                        <RadioGroup
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            name="row-radio-buttons-group"
+                            defaultValue={EnrollmentChoice.NotInterested}
+                            value={enrollmentChoice}
+                            sx={{
+                                display: 'grid',
+                                width: '100vw',
+                                maxWidth: '1080px',
+                                padding: '.2em',
+                                gridAutoFlow: 'column',
+                                gridAutoColumns: '1fr',
+                                placeContent: 'center',
+                            }}
+                            onChange={(e) => {
+                                handleEnrollmentChoiceChange(e);
+                            }}
+                        >
+                            <EnrollmentSelector
+                                sx={{ display: 'grid', textAlign: 'center', p: '.4em' }}
+                                value={EnrollmentChoice.NotInterested}
+                                disabled={!user}
+                                control={<Radio size="small" />}
+                                label="Ikke interessert"
+                            />
+                            <EnrollmentSelector
+                                value={EnrollmentChoice.IfIHaveTo}
+                                disabled={!user}
+                                sx={{ display: 'grid', backgroundColor: '#00000055', textAlign: 'center', p: '.4em' }}
+                                control={<Radio size="small" />}
+                                label="Hvis jeg må"
+                            />
+                            <EnrollmentSelector
+                                value={EnrollmentChoice.Interested}
+                                disabled={!user}
+                                sx={{ display: 'grid', backgroundColor: '#000000aa', textAlign: 'center', p: '.4em' }}
+                                control={<Radio size="small" />}
+                                label="Har lyst"
+                            />
+                            <EnrollmentSelector
+                                value={EnrollmentChoice.VeryInterested}
+                                disabled={!user}
+                                control={<Radio size="small" />}
+                                label="Har veldig lyst"
+                                sx={{ display: 'grid', backgroundColor: '#000000ff', textAlign: 'center', p: '.4em' }}
+                            />
+                        </RadioGroup>
+                    </FormControl>
+                </CardContent>
+                <Divider />
+            </Card>
+            <Dialog open={openLogin}>
+                <Login setChoice={ () => setOpenLogin(false) } />
+            </Dialog>
+        </>
     );
 };
 
