7f07dfde3c39be37bff23fd3cdbe7609d11c92e9
Christer M.L. Bendixen <gitstuff@fortelle.com>
2023-09-24 11:34:55 +0200

    stailet EditDialog - inkludert at vi gjemmer previewen på små skjermer

--- con-organizer/components/EventUi.tsx
+++ con-organizer/components/EventUi.tsx
@@ -77,9 +77,7 @@ const EventUi = ({ conEvent }: Props) => {
 
     return (
         <>
-            <Card
-                sx={{ background: 'linear-gradient(to left, black, transparent, transparent, black)', width: '100%' }}
-            >
+            <Card sx={{ background: 'linear-gradient(to left, black, #222, #222, black)', width: '100%' }}>
                 <EventHeader conEvent={conEvent} />
                 <Divider />
                 <Box
