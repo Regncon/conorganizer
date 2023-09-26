dff1c9e74697d177338d02f4fa57a163e562a7d3
CoLd As IcE <coldasice_t@hotmail.com>
2023-09-26 19:10:59 +0200

    Add: dynamic rout to api

--- con-organizer/app/api/getlogininfo/[id]/route.ts
+++ con-organizer/app/api/getlogininfo/[id]/route.ts
@@ -1,19 +1,20 @@
 import { type NextRequest, NextResponse } from 'next/server';
+import { adminDb, adminDoc } from '@/lib/firebaseAdmin';
 
 type Test = {
     message: string;
     error: Error;
 };
 
-export const GET = async (request: NextRequest, { params: { id } }: { params: { id: number } }) => {
+export const GET = async (request: NextRequest, { params: { id } }: { params: { id: string } }) => {
+    console.log(id);
+
     // query is "hello" for /api/search?query=hello
     const searchParams = request.nextUrl.searchParams;
+    const dbData = await adminDb.collection('usersettings').get();
+    console.log(dbData);
 
-    if (!id) {
-        return NextResponse.json({ message: 'NOOOO NOO U DONT' }, { status: 403 });
-    }
-
-    return NextResponse.json({ message: 'test' }, { status: 200 });
+    return NextResponse.json({ message: dbData }, { status: 200 });
 };
 
 export const POST = async (request: Request) => {
