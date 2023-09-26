f29f9236553258191e0a2f954bb3797a7fb88326
CoLd As IcE <coldasice_t@hotmail.com>
2023-09-26 14:11:24 +0200

    WIP: api some examples

--- con-organizer/app/api/getlogininfo/route.ts
+++ con-organizer/app/api/getlogininfo/route.ts
@@ -1,10 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
 
 type Test = {
     message: string;
     error: Error;
 };
 
-export const GET = async (req: NextRequest) => {
+export const GET = async (request: NextRequest, { params: { id } }: { params: { id: number } }) => {
+    // query is "hello" for /api/search?query=hello
+    const searchParams = request.nextUrl.searchParams;
+
+    if (!id) {
+        return NextResponse.json({ message: 'NOOOO NOO U DONT' }, { status: 403 });
+    }
+
     return NextResponse.json({ message: 'test' }, { status: 200 });
+};
+
+export const POST = async (request: Request) => {
+    const res = await request.json();
+    return NextResponse.json({ res });
 };
