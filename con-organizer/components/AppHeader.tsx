4cc0d54e13b1e9a043b8958efc0a47de9f98c6ae
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2023-10-01 19:43:13 +0200

    Removed help needed.

--- con-organizer/components/AppHeader.tsx
+++ con-organizer/components/AppHeader.tsx
@@ -1,7 +1,4 @@
-import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Accordion, AccordionDetails, AccordionSummary, Box, Link } from '@mui/material';
+import { Box } from '@mui/material';
 import { Typography } from '@/lib/mui';
 
 const AppHeader = () => {
@@ -20,28 +17,6 @@ const AppHeader = () => {
                     </Typography>
                     <Typography variant="h4">Program</Typography>
                 </div>
-                <Accordion sx={{ maxWidth: '20em', position: 'absolute', top: '0', right: '0', zIndex: '9000' }}>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography variant="caption">
-                            <FontAwesomeIcon icon={faUserPlus} color="#55cc99" />
-                            &nbsp; Regncon trenger deg!
-                        </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography variant="caption">
-                            Vi har rekordstor påmelding i år og trenger flere arrang&oslash;rer. Hvis du har noe du kan
-                            arrangere selv,&nbsp;
-                            <Link href="https://www.regncon.no/pamelding-av-arrangement/">meld det på her</Link>.
-                            &nbsp;Hvis ikke, kan du se etter arrangementer med dette symbolet:&nbsp;
-                            <FontAwesomeIcon icon={faUserPlus} color="#55cc99" /> og sende styret en epost hvis du vil
-                            hjelpe med å arrangere: <Link href="mailto:arrangere@regncon.no">arrangere@regncon.no</Link>
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
             </header>
         </Box>
     );
