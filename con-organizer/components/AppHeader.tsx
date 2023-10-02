43e8b76602d1a52df08987e075e320bd33ac89cb
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2023-10-02 10:31:48 +0200

    Added media query to header for mobile.

--- con-organizer/components/AppHeader.tsx
+++ con-organizer/components/AppHeader.tsx
@@ -1,9 +1,22 @@
+import * as React from 'react';
 import { Box } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { Typography } from '@/lib/mui';
 
 const AppHeader = () => {
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    console.log(isSmallScreen);
+
     return (
-        <Box sx={{ p: '5em 0 4em 1em', margin: '0 auto', maxWidth: '900px' }}>
+        <Box
+            sx={
+                isSmallScreen
+                    ? { p: '1em 0 1em 1em', margin: '0 auto', maxWidth: '900px' }
+                    : { p: '5em 0 4em 1em', margin: '0 auto', maxWidth: '600px' }
+            }
+        >
             <header className="AppHeader">
                 <img
                     src="/image/regnconlogony.png"
@@ -12,7 +25,7 @@ const AppHeader = () => {
                     onClick={() => (window.location.href = `/`)}
                 />
                 <div>
-                    <Typography variant="h1" color="white">
+                    <Typography variant={isSmallScreen ? 'h5' : 'h1'} color="white">
                         Regncon XXXI
                     </Typography>
                     <Typography variant="h4">Program</Typography>
