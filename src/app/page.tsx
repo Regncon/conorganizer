7308f8fa3f0d73d87bea30a6ee6d12d0614791a2
cirashrii <cirashrii@gmail.com>
2024-02-22 19:54:09 +0100

    Config actually set up ver 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
-import { Box, Button } from "@mui/material";
+import { Box, Button, CssBaseline, ThemeProvider } from "@mui/material";
+import { muiDark } from "@/lib/muiTheme";
 
 export default function Home() {
   return (
-    <Box component="main" className={styles.main}>
-      Hello world! :D
-    </Box>
+			<Box component="main" className={styles.main}>
+				Hello world! :D
+			</Box>
   );
 }
---
+++ src/lib/muiTheme.ts
@@ -0,0 +1,74 @@
+'use client';
+
+import { createTheme, ThemeOptions } from '@mui/material';
+import { EB_Garamond, Inter } from 'next/font/google';
+
+const Garamond = EB_Garamond({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    style: ['normal', 'italic'],
+    display: 'swap',
+});
+const inter = Inter({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    style: ['normal'],
+    display: 'swap',
+});
+
+const muiDarkTheme: ThemeOptions = {
+    palette: {
+        mode: 'dark',
+        primary: {
+            light: '#e0cfc9',
+            main: '#a1887f',
+            dark: '#000',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ffd54f',
+            main: '#ff8f00',
+            dark: '#000',
+            contrastText: '#000',
+        },
+    },
+    typography: {
+        h6: {
+            fontWeight: 'bold',
+        },
+        h1: {
+            fontWeight: '700',
+            fontFamily: Garamond.style.fontFamily,
+            fontSize: '2.7rem',
+        },
+        h2: {
+            fontWeight: '700',
+            fontFamily: Garamond.style.fontFamily,
+            color: 'grey',
+            fontStyle: 'italic',
+            fontSize: '2.5em',
+            textShadow: '0 0 1em black',
+        },
+        h3: {
+            fontWeight: '700',
+            lineHeight: '1',
+            fontSize: '2.2rem',
+            // textShadow: "0 0 1em black",
+            fontFamily: Garamond.style.fontFamily,
+        },
+        h4: {
+            color: '#ddd',
+            fontWeight: '400',
+            fontStyle: 'italic',
+            fontSize: '1.3rem',
+            // textShadow: "0 0 .7em black",
+            fontFamily: Garamond.style.fontFamily,
+        },
+        body1: {
+            fontFamily: inter.style.fontFamily,
+            fontSize: '1rem',
+        },
+    },
+};
+
+export const muiDark = createTheme(muiDarkTheme);
