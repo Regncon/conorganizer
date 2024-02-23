067ecc0038f638dc821775debc6f764000a0a050
CoLd As IcE <coldasice_t@hotmail.com>
2024-02-23 04:42:53 +0100

    Some testing :D

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
-import Image from 'next/image';
+import Container from '@mui/material/Container';
 import styles from './page.module.scss';
-import { Box, Button, CssBaseline, ThemeProvider } from '@mui/material';
-import { muiDark } from '@/lib/muiTheme';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 
 export default function Home() {
 	return (
-		<Box component="main" className={styles.main}>
-			Hello world! :D
-		</Box>
+		<Container
+			component={'main'}
+			maxWidth="xl"
+			sx={{
+				display: 'grid',
+				placeContent: 'center',
+			}}
+		>
+			<Box
+				component={Paper}
+				sx={{ display: 'grid', placeContent: 'center' }}
+				className={styles['main-test']}
+				elevation={1}
+				height={'20rem'}
+				width={'50rem'}
+			>
+				<Typography>Hello world! :D</Typography>
+			</Box>
+		</Container>
 	);
 }
