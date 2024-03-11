74f20ed8aa397ada76559011b34c22dcd2a25ce5
CoLdAs1cE <coldasice_t@hotmail.com>
2024-03-11 20:17:30 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import styles from './page.module.scss';
-import EventCardBig from './EventCardBig';
 
 export default function Home() {
 	return (
@@ -14,16 +9,6 @@ export default function Home() {
 				display: 'grid',
 				placeContent: 'center',
 			}}
-		>
-			<Box
-				component={Paper}
-				sx={{ display: 'grid', placeContent: 'center' }}
-				className={styles['main-test']}
-				elevation={1}
-			>
-			<img src="/placeholderlogo.png" alt="logo" />
-			<EventCardBig/>
-			</Box>
-		</Container>
+		></Container>
 	);
 }
