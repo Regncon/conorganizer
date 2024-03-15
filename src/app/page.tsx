6a06c441293cc7de4f935f132c9d5b61b755ad7e
M1Melk <skimmeland04@gmail.com>
2024-03-15 20:18:26 +0100

    import navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Container from '@mui/material/Container';
 import AppBar from '@mui/material/AppBar';
+import { Box } from '@mui/material';
+import navbar from './navbar';
+
 export default function Home() {
 	return (
 		<Container
