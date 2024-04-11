1a88faf4181f74518e2a7845491aee672d1a93fc
CoLdAs1cE <coldasice_t@hotmail.com>
2024-04-11 21:48:32 +0200

    Fix: Overlap Header and push bun lockfile

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -4,12 +4,25 @@ import NavigateBefore from '@mui/icons-material/NavigateBefore';
 import IconButton from '@mui/material/IconButton';
 
 const Event = () => {
-    const arrayet = ['katt', 'hund', 'fugl', 'hatt', 'nisse'];
+	const arrayet = ['katt', 'hund', 'fugl', 'hatt', 'nisse'];
 	return (
 		<Box sx={{ display: 'grid', gridTemplateAreas: '"header""content"' }}>
-			<Box sx={{ gridArea: 'header', display: 'grid', gridTemplateRows: '1fr 1fr' }}>
+			<Box
+				sx={{
+					gridArea: 'header',
+					display: 'grid',
+					'& > *': {
+						gridColumn: ' 1 / 2',
+						gridRow: ' 1 / 2',
+					},
+				}}
+			>
 				<Image src="/blekksprut2.jpg" alt="noe alt-tekst" width={375} height={230} />
-				<Box sx={{ background: 'linear-gradient(0deg, black, transparent)' }}>
+				<Box
+					sx={{
+						background: 'linear-gradient(0deg, black, transparent)',
+					}}
+				>
 					<IconButton>
 						<NavigateBefore />
 					</IconButton>
@@ -19,11 +32,15 @@ const Event = () => {
 			<Box sx={{ gridArea: 'content' }}>
 				<Box>
 					<Typography component="h2">GM: Fransibald von Fokkoff</Typography>
-					<Typography component="em" sx={{color:'orangered'}}>System: Mage - the ascension</Typography>
+					<Typography component="em" sx={{ color: 'orangered' }}>
+						System: Mage - the ascension
+					</Typography>
 				</Box>
-                <Box sx={{display:'flex', gap:'.5em'}}>
-                    {arrayet.map((vesen) => ( <Chip variant='outlined' label={vesen} key={vesen} icon={<NavigateBefore />} />))}
-                </Box>
+				<Box sx={{ display: 'flex', gap: '.5em' }}>
+					{arrayet.map((vesen) => (
+						<Chip variant="outlined" label={vesen} key={vesen} icon={<NavigateBefore />} />
+					))}
+				</Box>
 				<Typography component="p">
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo, illo quisquam. Quae odit impedit
 					est, odio nisi doloremque ullam alias magnam aspernatur error labore eligendi aliquid magni culpa
