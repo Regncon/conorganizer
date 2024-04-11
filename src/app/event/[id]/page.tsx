906cd2cd5d03a256b86d9258ead37c51bd48563a
Christer M.L. Bendixen <christer@fortelle.no>
2024-04-11 20:30:29 +0200

    see last commit

---
+++ src/app/event/[id]/page.tsx
@@ -0,0 +1,37 @@
+import { Box, Chip, Typography } from '@mui/material';
+import Image from 'next/image';
+import NavigateBefore from '@mui/icons-material/NavigateBefore';
+import IconButton from '@mui/material/IconButton';
+
+const Event = () => {
+    const arrayet = ['katt', 'hund', 'fugl', 'hatt', 'nisse'];
+	return (
+		<Box sx={{ display: 'grid', gridTemplateAreas: '"header""content"' }}>
+			<Box sx={{ gridArea: 'header', display: 'grid', gridTemplateRows: '1fr 1fr' }}>
+				<Image src="/blekksprut2.jpg" alt="noe alt-tekst" width={375} height={230} />
+				<Box sx={{ background: 'linear-gradient(0deg, black, transparent)' }}>
+					<IconButton>
+						<NavigateBefore />
+					</IconButton>
+					<Typography component="h1">Nei, du er en n00b!</Typography>
+				</Box>
+			</Box>
+			<Box sx={{ gridArea: 'content' }}>
+				<Box>
+					<Typography component="h2">GM: Fransibald von Fokkoff</Typography>
+					<Typography component="em" sx={{color:'orangered'}}>System: Mage - the ascension</Typography>
+				</Box>
+                <Box sx={{display:'flex', gap:'.5em'}}>
+                    {arrayet.map((vesen) => ( <Chip variant='outlined' label={vesen} key={vesen} icon={<NavigateBefore />} />))}
+                </Box>
+				<Typography component="p">
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo, illo quisquam. Quae odit impedit
+					est, odio nisi doloremque ullam alias magnam aspernatur error labore eligendi aliquid magni culpa
+					neque ad.
+				</Typography>
+			</Box>
+		</Box>
+	);
+};
+
+export default Event;
