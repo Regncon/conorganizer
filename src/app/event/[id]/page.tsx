ffc3480f32f53400d10810090ce358e513c3f718
CoLdAs1cE <coldasice_t@hotmail.com>
2024-04-27 06:24:38 +0200

    More styling changes typography and others

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,11 +1,12 @@
 'use client';
-import { Box, Chip, Slider, Typography } from '@mui/material';
+import { Box, Chip, Slider, Typography, chipClasses, sliderClasses, type SxProps, type Theme } from '@mui/material';
 import Image from 'next/image';
 import NavigateBefore from '@mui/icons-material/NavigateBefore';
 import IconButton from '@mui/material/IconButton';
 import blekksprut2 from '$public/blekksprut2.jpg';
 import HelpIcon from '@mui/icons-material/Help';
 import { useState } from 'react';
+import Link from 'next/link';
 
 const marks = [
 	{
@@ -29,6 +30,9 @@ const marks = [
 const Event = () => {
 	const arrayet = ['katt', 'hund', 'fugl', 'rollespill', 'nisse', 'nisse', 'nisse', 'nisse', 'nisse'];
 	const [interest, setInterest] = useState<number>(0);
+
+	const paragraphStyle: SxProps<Theme> = { margin: '1rem 0' };
+	const strongStyle: SxProps<Theme> = { fontWeight: 700 };
 	return (
 		<Box>
 			<Box
@@ -78,62 +82,111 @@ const Event = () => {
 					</Box>
 				</Box>
 			</Box>
-			<Box>
-				<Box sx={{ display: 'flex', gap: '1rem', placeContent: 'center' }}>
-					<Box>
-						<Typography component="span" sx={{ color: 'secondary.contrastText' }}>
-							{arrayet.includes('rollespill') ? 'Gamemaster' : 'Arrangør'}
-						</Typography>
-						<Typography variant="h2">Fransibald von Fokkoff</Typography>
-					</Box>
-					<Box>
-						<Typography component="span" sx={{ color: 'secondary.contrastText' }}>
-							System
-						</Typography>
-						<Typography variant="h2">Mage - the ascension</Typography>
-					</Box>
+
+			<Box sx={{ display: 'flex', gap: '1rem', placeContent: 'center', marginBottom: 1 }}>
+				<Box>
+					<Typography component="span" sx={{ color: 'secondary.contrastText' }}>
+						{arrayet.includes('rollespill') ? 'Gamemaster' : 'Arrangør'}
+					</Typography>
+					<Typography variant="h2">Fransibald von Fokkoff</Typography>
 				</Box>
-				<Box sx={{ display: 'flex', gap: '.5em', overflowX: 'auto' }}>
-					{arrayet.map((vesen) => (
-						<Chip variant="outlined" label={vesen} key={vesen} icon={<NavigateBefore />} />
-					))}
-				</Box>
-				<Box
-					sx={{
-						backgroundColor: 'secondary.contrastText',
-						minHeight: '62px',
-						textAlign: 'center',
-						display: 'grid',
-						placeContent: 'center',
-						borderRadius: '0.2rem',
-					}}
-				>
-					<Typography component="p">{marks[interest].label}</Typography>
-				</Box>
-				<Box sx={{ padding: '0 0.35rem' }}>
-					<Slider
-						onChange={(e) => {
-							const target = e.target as HTMLInputElement;
-							setInterest(Number(target.value));
-						}}
-						aria-label="Temperature"
-						marks
-						defaultValue={0}
-						valueLabelDisplay="off"
-						shiftStep={1}
-						step={1}
-						min={0}
-						max={3}
-					/>
+				<Box>
+					<Typography component="span" sx={{ color: 'secondary.contrastText' }}>
+						System
+					</Typography>
+					<Typography variant="h2">Mage - the ascension</Typography>
 				</Box>
 			</Box>
-			<Box display="inline-flex" gap="0.4rem">
-				<HelpIcon sx={{ scale: '1.5' }} />
+			<Box
+				sx={{
+					display: 'flex',
+					gap: '.5em',
+					overflowX: 'auto',
+					paddingBottom: 1,
+				}}
+			>
+				{arrayet.map((vesen) => (
+					<Chip
+						variant="outlined"
+						label={vesen}
+						key={vesen}
+						sx={{
+							[`&.${chipClasses.root} .${chipClasses.icon}, &`]: {
+								color: 'secondary.contrastText',
+								borderColor: 'secondary.contrastText',
+							},
+						}}
+						icon={<NavigateBefore />}
+					/>
+				))}
+			</Box>
+			<Box
+				sx={{
+					backgroundColor: 'secondary.contrastText',
+					minHeight: '62px',
+					textAlign: 'center',
+					display: 'grid',
+					placeContent: 'center',
+					borderRadius: '0.2rem',
+				}}
+			>
+				<Typography sx={paragraphStyle} component="p">
+					{marks[interest].label}
+				</Typography>
+			</Box>
+			<Box sx={{ padding: '0 0.35rem' }}>
+				<Slider
+					onChange={(e) => {
+						const target = e.target as HTMLInputElement;
+						setInterest(Number(target.value));
+					}}
+					sx={{
+						color: 'secondary.contrastText',
+						[`.${sliderClasses.rail}`]: {
+							backgroundColor: '#3d3b3b',
+							height: '1rem',
+						},
+						[`.${sliderClasses.track}`]: {
+							height: '1rem',
+						},
+						[`.${sliderClasses.mark}`]: {
+							height: '0.8rem',
+							width: '0.8rem',
+							borderRadius: '50%',
+						},
+						[`.${sliderClasses.markActive}`]: {
+							backgroundColor: 'secondary.contrastText',
+						},
+					}}
+					marks
+					defaultValue={0}
+					valueLabelDisplay="off"
+					min={0}
+					max={3}
+				/>
+			</Box>
+
+			<Box
+				component={Link}
+				href="#"
+				sx={{
+					display: 'inline-flex',
+					gap: '0.4rem',
+					marginBottom: 2,
+					paddingLeft: 1,
+					color: 'secondary.contrastText',
+				}}
+			>
+				<HelpIcon sx={{ scale: '1.5', placeSelf: 'center' }} />
 				<Typography component="p">Forvirret? Les mer om påmeldingssystemet</Typography>
 			</Box>
-			<Typography component="strong">Lorem ipsum dolor, sit amet consectetur</Typography>
-			<Typography component="strong">adipisicing elit. Nemo, illo quisquam. Quae odit impedit </Typography>
-			<Typography component="p">
+			<Typography sx={strongStyle} component="strong">
+				Lorem ipsum dolor, sit amet consectetur
+			</Typography>
+			<Typography sx={strongStyle} component="strong">
+				adipisicing elit. Nemo, illo quisquam. Quae odit impedit{' '}
+			</Typography>
+			<Typography sx={{ ...paragraphStyle, marginBottom: 0, paddingBlockEnd: '1rem' }} component="p">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus distinctio quia odit recusandae nobis
 				autem, odio id pariatur magnam illo saepe laborum nesciunt quasi doloremque provident neque eligendi,
 				quisquam quas?
