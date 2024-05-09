a63a23d5eae8a4fe0674d7845fe1f27596d611ba
cirashrii <cirashrii@gmail.com>
2024-05-09 19:45:26 +0200

    Form complete

--- src/app/event/create/page.tsx
+++ src/app/event/create/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
 	Box,
 	Card,
@@ -11,13 +12,21 @@ import {
 	FormLabel,
 	Radio,
 	RadioGroup,
+	TextareaAutosize,
+	FormGroup,
+	Checkbox,
+	Button,
 } from '@mui/material';
 import { Unstable_NumberInput as NumberInput } from '@mui/base/Unstable_NumberInput';
 import CustomNumberInput from './CustomNumberInput';
+import ConfettiExplosion from 'react-confetti-explosion';
+import { useState } from 'react';
 
 const Create = () => {
+	const [isExploding,setIsExploding] = useState(false);
 	return (
 		<form>
+			{isExploding && <ConfettiExplosion force={0.8} duration={3000} particleCount={1000} width={1000} />}
 			<Paper>
 				<Typography variant="h1">Meld på arrangement til Regncon XXXII 2024</Typography>
 				<Typography>
@@ -70,7 +79,53 @@ const Create = () => {
 				<NumberInput />
 			</Paper>
 			<Paper>
-				<TextField name="system" label="Spillsystem" variant="outlined" />
+				<FormControl>
+					<FormLabel>Skildring av modulen (tekst til programmet):</FormLabel>
+					<TextareaAutosize minRows={3} name="description" />
+				</FormControl>
+			</Paper>
+			<Paper>
+				<FormGroup>
+					<FormLabel>Kva for pulje kan du arrangere i?</FormLabel>
+					<FormControlLabel control={<Checkbox defaultChecked />} label="Fredag Kveld" />
+					<FormControlLabel control={<Checkbox defaultChecked />} label="Lørdag Morgen" />
+					<FormControlLabel control={<Checkbox defaultChecked />} label="Lørdag Kveld" />
+					<FormControlLabel control={<Checkbox defaultChecked />} label="Søndag Morgen" />
+				</FormGroup>
+			</Paper>
+			<Paper>
+				<FormGroup>
+					<FormLabel>Kryss av for det som gjeld</FormLabel>
+					<FormControlLabel
+						control={<Checkbox />}
+						label="Eg vil vere med på modulkonkurransen husk å sende modulen til moduler@regncon.no innen første september!)"
+					/>
+					<FormControlLabel control={<Checkbox />} label="Arrangementet passer for barn" />
+					<FormControlLabel control={<Checkbox />} label="Arrangementet passer berre for vaksne (18+)" />
+					<FormControlLabel control={<Checkbox />} label="Arrangementet er nybyrjarvenleg" />
+					<FormControlLabel control={<Checkbox />} label="Arrangementet kan haldast på engelsk" />
+					<FormControlLabel control={<Checkbox />} label="Andre kan halda arrangementet" />
+					<FormControlLabel
+						control={<Checkbox />}
+						label="Eg trur arrangementet vil vare kortare enn 3 timer"
+					/>
+					<FormControlLabel
+						control={<Checkbox />}
+						label="Eg trur arrangementet vil vare lenger enn 6 timer"
+					/>
+				</FormGroup>
+			</Paper>
+			<Paper>
+				<FormControl>
+					<FormLabel>Merknader: Er det noko anna du vil vi skal vite?</FormLabel>
+					<TextareaAutosize minRows={3} name="description" />
+				</FormControl>
+			</Paper>
+			<Paper>
+				<Typography>
+					Skjemaet vert lagra automatisk, men om du likevel vil trykke på ein knapp, så er det ein her. :)
+				</Typography>
+				<Button onClick={() => setIsExploding(!isExploding)}>Send inn</Button>
 			</Paper>
 		</form>
 	);
