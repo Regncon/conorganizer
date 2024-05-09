d24835185df8f0247342d8f3833f1e4f475de77e
cirashrii <cirashrii@gmail.com>
2024-05-09 17:29:04 +0200

    Created create event page

---
+++ src/app/event/create/page.tsx
@@ -0,0 +1,17 @@
+import { Box, Card, Typography } from '@mui/material';
+
+const Create = () => {
+	return (
+	<Box>
+		<Card>
+			<Typography>
+				Takk for at du vil arrangere eit spel på Regncon, anten det er brettspel, kortspel, rollespel eller
+				anna, så sett vi enormt pris på ditt bidrag. Fyll inn skjemaet så godt du kan, og ikkje vere redd for å
+				ta kontakt med Regnconstyret på regncon@gmail.com om du skulle lure på noko!
+			</Typography>
+		</Card>
+	</Box>
+    );
+};
+
+export default Create;
