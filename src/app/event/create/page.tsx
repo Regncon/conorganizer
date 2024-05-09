87e05fdbe105b7a425a63e8257ee09c432867ce8
CoLdAs1cE <coldasice_t@hotmail.com>
2024-05-10 01:05:58 +0200

    Realtime updating database as form fills out

--- src/app/event/create/page.tsx
+++ src/app/event/create/page.tsx
@@ -1,133 +1,237 @@
 'use client';
-import {
-	Box,
-	Card,
-	Typography,
-	CardContent,
-	Paper,
-	Input,
-	TextField,
-	FormControl,
-	FormControlLabel,
-	FormLabel,
-	Radio,
-	RadioGroup,
-	TextareaAutosize,
-	FormGroup,
-	Checkbox,
-	Button,
-} from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import FormGroup from '@mui/material/FormGroup';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
 import { Unstable_NumberInput as NumberInput } from '@mui/base/Unstable_NumberInput';
-import CustomNumberInput from './CustomNumberInput';
-import ConfettiExplosion from 'react-confetti-explosion';
-import { useState } from 'react';
+import Confetti from 'react-confetti'
+import { useEffect, useState } from 'react';
+import { doc, onSnapshot, setDoc } from 'firebase/firestore';
+import { db } from '$lib/firebase/firebase';
+import type { NewEvent } from '$app/types';
 
 const Create = () => {
 	const [isExploding, setIsExploding] = useState(false);
-	return (
-		<form>
-			{isExploding && <ConfettiExplosion force={0.8} duration={3000} particleCount={250} width={1000} />}
-			<Paper>
-				<Typography variant="h1">Meld på arrangement til Regncon XXXII 2024</Typography>
-				<Typography>
-					Takk for at du vil arrangere eit spel på Regncon, anten det er brettspel, kortspel, rollespel eller
-					anna, så sett vi enormt pris på ditt bidrag. Fyll inn skjemaet så godt du kan, og ikkje vere redd
-					for å ta kontakt med Regnconstyret på <a href="mailto:regncon@gmail.com">regncon@gmail.com</a>
-					om du skulle lure på noko!
-				</Typography>
-			</Paper>
-			<Paper>
-				<TextField type="email" name="email" label="E-postadresse" variant="outlined" required />
-			</Paper>
-			<Paper>
-				<TextField name="name" label="Arrangørens namn (Ditt namn)" variant="outlined" required />
-			</Paper>
-			<Paper>
-				<TextField
-					type="phone"
-					name="phone"
-					label="Kva telefonnummer kan vi nå deg på?"
-					variant="outlined"
-					required
-				/>
-			</Paper>
-			<Paper>
-				<TextField
-					name="title"
-					label="Tittel på spelmodul / arrangement
-"
-					variant="outlined"
-					required
-				/>
-			</Paper>
-			<Paper>
-				<TextField name="system" label="Spillsystem" variant="outlined" />
-			</Paper>
-			<Paper>
-				<FormControl>
-					<FormLabel>Kva type spel er det?</FormLabel>
-					<RadioGroup defaultValue="rollespel" name="radio-buttons-group">
-						<FormControlLabel name="rollespel" value="rollespel" control={<Radio />} label="Rollespel" />
-						<FormControlLabel name="brettspel" value="brettspel" control={<Radio />} label="Brettspel" />
-						<FormControlLabel name="kortspel" value="kortspel" control={<Radio />} label="Kortspel" />
-						<FormControlLabel name="annet" value="annet" control={<Radio />} label="Annet" />
-					</RadioGroup>
-				</FormControl>
-			</Paper>
+	const newEventDocRef = doc(db, 'users', 'HESPa0eFhPWWjGBGEXw6HjB1r8v1', 'myEvents', '72g29OPy3b9cCpmzg4R2');
+	const [newEvent, setNewEvent] = useState<NewEvent>();
+	console.log(newEvent);
 
-			<Paper>
-				<NumberInput />
-			</Paper>
-			<Paper>
-				<FormControl>
-					<FormLabel>Skildring av modulen (tekst til programmet):</FormLabel>
-					<TextareaAutosize minRows={3} name="description" />
-				</FormControl>
-			</Paper>
-			<Paper>
-				<FormGroup>
-					<FormLabel>Kva for pulje kan du arrangere i?</FormLabel>
-					<FormControlLabel control={<Checkbox defaultChecked />} label="Fredag Kveld" />
-					<FormControlLabel control={<Checkbox defaultChecked />} label="Lørdag Morgen" />
-					<FormControlLabel control={<Checkbox defaultChecked />} label="Lørdag Kveld" />
-					<FormControlLabel control={<Checkbox defaultChecked />} label="Søndag Morgen" />
-				</FormGroup>
-			</Paper>
-			<Paper>
-				<FormGroup>
-					<FormLabel>Kryss av for det som gjeld</FormLabel>
-					<FormControlLabel
-						control={<Checkbox />}
-						label="Eg vil vere med på modulkonkurransen husk å sende modulen til moduler@regncon.no innen første september!)"
+	useEffect(() => {
+		const unsubscribe = onSnapshot(newEventDocRef, (snapshot) => {
+			setNewEvent(snapshot.data() as NewEvent);
+			console.log(snapshot, 'snapshot');
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	}, []);
+
+	return newEvent ?
+			<form
+				onChange={(e) => {
+					const { value: inputValue, name: inputName, checked, type } = e.target as HTMLInputElement;
+
+					let value: string | boolean = inputValue;
+					let name: string = inputName;
+
+					if (type === 'checkbox') {
+						value = checked;
+					}
+
+					if (type === 'radio') {
+						name = 'gameType';
+						value = inputName;
+					}
+
+					setDoc(newEventDocRef, { ...newEvent, [name]: value });
+				}}
+			>
+				{isExploding && (
+					<Confetti
+						onConfettiComplete={() => {
+							setIsExploding(!isExploding);
+						}}
+						numberOfPieces={2000}
+						recycle={false}
+						height={document.body.scrollHeight}
+						width={document.body.scrollWidth}
 					/>
-					<FormControlLabel control={<Checkbox />} label="Arrangementet passer for barn" />
-					<FormControlLabel control={<Checkbox />} label="Arrangementet passer berre for vaksne (18+)" />
-					<FormControlLabel control={<Checkbox />} label="Arrangementet er nybyrjarvenleg" />
-					<FormControlLabel control={<Checkbox />} label="Arrangementet kan haldast på engelsk" />
-					<FormControlLabel control={<Checkbox />} label="Andre kan halda arrangementet" />
-					<FormControlLabel
-						control={<Checkbox />}
-						label="Eg trur arrangementet vil vare kortare enn 3 timer"
+				)}
+				<Paper>
+					<Typography variant="h1">Meld på arrangement til Regncon XXXII 2024</Typography>
+					<Typography>
+						Takk for at du vil arrangere eit spel på Regncon, anten det er brettspel, kortspel, rollespel
+						eller anna, så sett vi enormt pris på ditt bidrag. Fyll inn skjemaet så godt du kan, og ikkje
+						vere redd for å ta kontakt med Regnconstyret på{' '}
+						<a href="mailto:regncon@gmail.com">regncon@gmail.com</a>
+						om du skulle lure på noko!
+					</Typography>
+				</Paper>
+				<Paper>
+					<TextField
+						type="email"
+						name="email"
+						value={newEvent.email}
+						label="E-postadresse"
+						variant="outlined"
+						required
 					/>
-					<FormControlLabel
-						control={<Checkbox />}
-						label="Eg trur arrangementet vil vare lenger enn 6 timer"
+				</Paper>
+				<Paper>
+					<TextField
+						name="name"
+						value={newEvent.name}
+						label="Arrangørens namn (Ditt namn)"
+						variant="outlined"
+						required
 					/>
-				</FormGroup>
-			</Paper>
-			<Paper>
-				<FormControl>
-					<FormLabel>Merknader: Er det noko anna du vil vi skal vite?</FormLabel>
-					<TextareaAutosize minRows={3} name="description" />
-				</FormControl>
-			</Paper>
-			<Paper>
-				<Typography>
-					Skjemaet vert lagra automatisk, men om du likevel vil trykke på ein knapp, så er det ein her. :)
-				</Typography>
-				<Button onClick={() => setIsExploding(!isExploding)}>Send inn</Button>
-			</Paper>
-		</form>
-	);
+				</Paper>
+				<Paper>
+					<TextField
+						type="phone"
+						name="phone"
+						value={newEvent.phone}
+						label="Kva telefonnummer kan vi nå deg på?"
+						variant="outlined"
+						required
+					/>
+				</Paper>
+				<Paper>
+					<TextField
+						name="title"
+						label="Tittel på spelmodul / arrangement"
+						value={newEvent.title}
+						variant="outlined"
+						required
+					/>
+				</Paper>
+				<Paper>
+					<TextField name="system" label="Spillsystem" value={newEvent.system} variant="outlined" />
+				</Paper>
+				<Paper>
+					<FormControl>
+						<FormLabel>Kva type spel er det?</FormLabel>
+						<RadioGroup
+							value={newEvent.gameType}
+							aria-labelledby="demo-controlled-radio-buttons-group"
+							name="controlled-radio-buttons-group"
+						>
+							<FormControlLabel
+								value="rolePlaying"
+								control={<Radio name="rolePlaying" />}
+								label="rollespel"
+							/>
+							<FormControlLabel
+								value="boardGame"
+								control={<Radio name="boardGame" />}
+								label="Brettspel"
+							/>
+							<FormControlLabel value="cardGame" control={<Radio name="cardGame" />} label="Kortspel" />
+							<FormControlLabel value="other" control={<Radio name="other" />} label="Annet" />
+						</RadioGroup>
+					</FormControl>
+				</Paper>
+
+				<Paper>
+					<NumberInput
+						value={newEvent.participants}
+						slotProps={{
+							input: {
+								name: 'participants',
+							},
+						}}
+					/>
+				</Paper>
+				<Paper>
+					<FormControl>
+						<FormLabel>Skildring av modulen (tekst til programmet):</FormLabel>
+						<TextareaAutosize minRows={3} name="description" value={newEvent.description} />
+					</FormControl>
+				</Paper>
+				<Paper>
+					<FormGroup>
+						<FormLabel>Kva for pulje kan du arrangere i?</FormLabel>
+						<FormControlLabel
+							control={<Checkbox checked={newEvent.fridayEvening} />}
+							name="fridayEvening"
+							label="Fredag Kveld"
+						/>
+						<FormControlLabel
+							control={<Checkbox checked={newEvent.saturdayMorning} />}
+							name="saturdayMorning"
+							label="Lørdag Morgen"
+						/>
+						<FormControlLabel
+							control={<Checkbox checked={newEvent.saturdayEvening} />}
+							name="saturdayEvening"
+							label="Lørdag Kveld"
+						/>
+						<FormControlLabel
+							control={<Checkbox checked={newEvent.sundayMorning} />}
+							name="sundayMorning"
+							label="Søndag Morgen"
+						/>
+					</FormGroup>
+				</Paper>
+				<Paper>
+					<FormGroup>
+						<FormLabel>Kryss av for det som gjeld</FormLabel>
+						<FormControlLabel
+							control={<Checkbox name="moduleCompetition" checked={newEvent.moduleCompetition} />}
+							label="Eg vil vere med på modulkonkurransen husk å sende modulen til moduler@regncon.no innen første september!)"
+						/>
+						<FormControlLabel
+							control={<Checkbox name="childFriendly" checked={newEvent.childFriendly} />}
+							label="Arrangementet passer for barn"
+						/>
+						<FormControlLabel
+							control={<Checkbox name="adultsOnly" checked={newEvent.adultsOnly} />}
+							label="Arrangementet passer berre for vaksne (18+)"
+						/>
+						<FormControlLabel
+							control={<Checkbox name="beginnerFriendly" checked={newEvent.beginnerFriendly} />}
+							label="Arrangementet er nybyrjarvenleg"
+						/>
+						<FormControlLabel
+							control={<Checkbox name="possiblyEnglish" checked={newEvent.possiblyEnglish} />}
+							label="Arrangementet kan haldast på engelsk"
+						/>
+						<FormControlLabel
+							control={<Checkbox name="volunteersPossible" checked={newEvent.volunteersPossible} />}
+							label="Andre kan halda arrangementet"
+						/>
+						<FormControlLabel
+							control={<Checkbox name="lessThanThreeHours" checked={newEvent.lessThanThreeHours} />}
+							label="Eg trur arrangementet vil vare kortare enn 3 timer"
+						/>
+						<FormControlLabel
+							control={<Checkbox name="moreThanSixHours" checked={newEvent.moreThanSixHours} />}
+							label="Eg trur arrangementet vil vare lenger enn 6 timer"
+						/>
+					</FormGroup>
+				</Paper>
+				<Paper>
+					<FormControl>
+						<FormLabel>Merknader: Er det noko anna du vil vi skal vite?</FormLabel>
+						<TextareaAutosize minRows={3} name="additionalComments" value={newEvent.additionalComments} />
+					</FormControl>
+				</Paper>
+				<Paper>
+					<Typography>
+						Skjemaet vert lagra automatisk, men om du likevel vil trykke på ein knapp, så er det ein her. :)
+					</Typography>
+					<Button onClick={() => setIsExploding(!isExploding)}>Send inn</Button>
+				</Paper>
+			</form>
+		:	null;
 };
 export default Create;
--- src/app/types.ts
+++ src/app/types.ts
@@ -7,10 +7,35 @@ export type EventCardProps = {
 };
 
 export type Event = {
-	id: 'string';
+	id: string;
 	title: string;
 	gameMaster: string;
 	system: string;
 	shortDescription: string;
 	icons?: string[];
+};
+
+export type NewEvent = {
+	id: string;
+	email: string;
+	name: string;
+	phone: string;
+	title: string;
+	system: string;
+	gameType: string;
+	participants: number;
+	fridayEvening: boolean;
+	saturdayMorning: boolean;
+	saturdayEvening: boolean;
+	sundayMorning: boolean;
+	moduleCompetition: boolean;
+	childFriendly: boolean;
+	possiblyEnglish: boolean;
+	adultsOnly: boolean;
+	volunteersPossible: boolean;
+	lessThanThreeHours: boolean;
+	moreThanSixHours: boolean;
+	beginnerFriendly: boolean;
+	description: string[];
+	additionalComments: string;
 };
