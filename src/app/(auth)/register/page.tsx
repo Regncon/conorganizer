db2a6512a2d678d9872a665b0f06b172b1a8b477
CoLdAs1cE <coldasice_t@hotmail.com>
2024-05-12 18:11:30 +0200

    Refactor and route goes to dashboard

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,72 +1,55 @@
 'use client';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { Button, Container, InputAdornment, Paper, TextField } from '@mui/material';
+import { Button, Container, Paper } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import { singUpAndCreateCookie, type RegisterDetails } from '$lib/firebase/firebase';
 import PasswordTextField from '../login/PasswordTextField';
 import { useRef } from 'react';
 import { useRouter } from 'next/navigation';
+import { emailRegExp } from '../shared/utils';
+import EmailField from '../shared/ui/EmailField';
 
 const Register = () => {
-	const passwordRef = useRef<HTMLInputElement>(null);
-	const confirmPasswordRef = useRef<HTMLInputElement>(null);
-	const formRef = useRef<HTMLFormElement>(null);
-	const emailRegExp = /^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+.[a-zA-Z]{2,4}$/;
-	const router = useRouter();
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const confirmPasswordRef = useRef<HTMLInputElement>(null);
+    const formRef = useRef<HTMLFormElement>(null);
 
-	return (
-		<Container component={Paper} fixed maxWidth="xl" sx={{ height: '70dvh' }}>
-			<Grid2
-				ref={formRef}
-				component="form"
-				container
-				sx={{ placeContent: 'center', height: '100%', flexDirection: 'column', gap: '1rem' }}
-				onSubmit={(e) => {
-					e.preventDefault();
-					const { password, confirm, email } = Object.fromEntries(
-						new FormData(e.target as HTMLFormElement)
-					) as RegisterDetails;
+    const router = useRouter();
 
-					if (password !== confirm) {
-						console.log('no match', password !== confirm);
-					}
+    return (
+        <Container component={Paper} fixed maxWidth="xl" sx={{ height: '70dvh' }}>
+            <Grid2
+                ref={formRef}
+                component="form"
+                container
+                sx={{ placeContent: 'center', height: '100%', flexDirection: 'column', gap: '1rem' }}
+                onSubmit={async (e) => {
+                    e.preventDefault();
+                    const { password, confirm, email } = Object.fromEntries(
+                        new FormData(e.target as HTMLFormElement)
+                    ) as RegisterDetails;
 
-					if (emailRegExp.test(email)) {
-						singUpAndCreateCookie(e);
-						router.push('/my-events');
-					}
-				}}
-			>
-				<TextField
-					type="email"
-					name="email"
-					autoComplete="email"
-					label="e-post"
-					variant="outlined"
-					required
-					InputProps={{
-						endAdornment: (
-							<InputAdornment position="end">
-								<AccountCircleIcon />
-							</InputAdornment>
-						),
-					}}
-					inputProps={{
-						pattern: emailRegExp.source,
-						title: 'epost@example.com',
-					}}
-				/>
-				<PasswordTextField autoComplete="new-password" ref={passwordRef} />
-				<PasswordTextField
-					autoComplete="new-password"
-					label="bekreft passord"
-					name="confirm"
-					ref={confirmPasswordRef}
-				/>
-				<Button type="submit">Log inn</Button>
-			</Grid2>
-		</Container>
-	);
+                    if (password !== confirm) {
+                        console.log('no match', password !== confirm);
+                    }
+
+                    if (emailRegExp.test(email)) {
+                        await singUpAndCreateCookie(e);
+                        router.push('/dashboard');
+                    }
+                }}
+            >
+                <EmailField />
+                <PasswordTextField autoComplete="new-password" ref={passwordRef} />
+                <PasswordTextField
+                    autoComplete="new-password"
+                    label="bekreft passord"
+                    name="confirm"
+                    ref={confirmPasswordRef}
+                />
+                <Button type="submit">Log inn</Button>
+            </Grid2>
+        </Container>
+    );
 };
 
 export default Register;
