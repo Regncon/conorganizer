810cc4eb8860a0dd46e50c5355b8ef6335961499
CoLdAs1cE <coldasice_t@hotmail.com>
2024-05-17 02:18:13 +0200

    Fix: h1-h3 font sizes

--- src/lib/muiTheme.ts
+++ src/lib/muiTheme.ts
@@ -3,68 +3,68 @@ import { createTheme, ThemeOptions } from '@mui/material';
 import { EB_Garamond, Raleway } from 'next/font/google';
 
 const Garamond = EB_Garamond({
-	weight: ['400', '700'],
-	subsets: ['latin'],
-	style: ['normal', 'italic'],
-	display: 'swap',
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    style: ['normal', 'italic'],
+    display: 'swap',
 });
 const raleway = Raleway({
-	weight: ['500', '700'],
-	subsets: ['latin'],
-	style: ['normal'],
-	display: 'swap',
+    weight: ['500', '700'],
+    subsets: ['latin'],
+    style: ['normal'],
+    display: 'swap',
 });
 
 const muiDarkTheme: ThemeOptions = {
-	palette: {
-		mode: 'dark',
-		primary: {
-			light: '#e0cfc9',
-			main: '#a1887f',
-			dark: '#000',
-			contrastText: '#fff',
-		},
-		secondary: {
-			light: '#ffd54f',
-			main: '#ff8f00',
-			dark: '#000',
-			contrastText: '#FF7979',
-		},
-	},
-	typography: {
-		h6: {
-			fontWeight: 'bold',
-		},
-		h1: {
-			fontWeight: '700',
-			fontFamily: Garamond.style.fontFamily,
-			fontSize: '2.7rem',
-		},
-		h2: {
-			fontWeight: '700',
-			fontFamily: Garamond.style.fontFamily,
-			fontSize: '2rem',
-		},
-		h3: {
-			fontWeight: '700',
-			lineHeight: '1',
-			fontSize: '2.2rem',
-			// textShadow: "0 0 1em black",
-			fontFamily: Garamond.style.fontFamily,
-		},
-		h4: {
-			color: '#ddd',
-			fontWeight: '400',
-			fontStyle: 'italic',
-			fontSize: '1.3rem',
-			// textShadow: "0 0 .7em black",
-			fontFamily: Garamond.style.fontFamily,
-		},
-		body1: {
-			fontFamily: raleway.style.fontFamily,
-			fontSize: '1rem',
-		},
-	},
+    palette: {
+        mode: 'dark',
+        primary: {
+            light: '#e0cfc9',
+            main: '#a1887f',
+            dark: '#000',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ffd54f',
+            main: '#ff8f00',
+            dark: '#000',
+            contrastText: '#FF7979',
+        },
+    },
+    typography: {
+        h6: {
+            fontWeight: 'bold',
+        },
+        h1: {
+            fontWeight: '700',
+            fontFamily: Garamond.style.fontFamily,
+            fontSize: '2.7rem',
+        },
+        h2: {
+            fontWeight: '700',
+            fontFamily: Garamond.style.fontFamily,
+            fontSize: '2.3rem',
+        },
+        h3: {
+            fontWeight: '700',
+            lineHeight: '1',
+            fontSize: '1.8rem',
+            // textShadow: "0 0 1em black",
+            fontFamily: Garamond.style.fontFamily,
+        },
+        h4: {
+            color: '#ddd',
+            fontWeight: '400',
+            fontStyle: 'italic',
+            fontSize: '1.3rem',
+            // textShadow: "0 0 .7em black",
+            fontFamily: Garamond.style.fontFamily,
+        },
+        body1: {
+            fontFamily: raleway.style.fontFamily,
+            fontSize: '1rem',
+        },
+    },
 };
 
 export const muiDark = createTheme(muiDarkTheme);
