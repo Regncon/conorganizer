4be6ef912932d42bd97001ebfeb47d590fe9c657
CoLdAs1cE <coldasice_t@hotmail.com>
2024-05-18 00:19:12 +0200

    Fix: redirect after 4 seconds and gives message that e-mail is sent and will be redirected and removed deleted file from .gitignore

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -4,7 +4,7 @@ import EmailTextField from '../shared/EmailTextField';
 import Button from '@mui/material/Button';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { forgotPassword } from '$lib/firebase/firebase';
-import { useTransition, type ComponentProps } from 'react';
+import { useState, useTransition, type ComponentProps } from 'react';
 import Container from '@mui/material/Container';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import { Typography } from '@mui/material';
@@ -13,15 +13,23 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons/faSpinner';
 
 const ForgotPassword = () => {
     const [isPending, startTransition] = useTransition();
+    const [message, setMessage] = useState<string>('');
     const router = useRouter();
     const searchParams = useSearchParams();
     const searchParamEmail = searchParams.get('email') ?? undefined;
     const handleSubmit: ComponentProps<'form'>['onSubmit'] = async (e) => {
         e.preventDefault();
         const { email } = Object.fromEntries(new FormData(e.target as HTMLFormElement)) as { email: string };
+        setMessage(
+            'Ein lenkje for å tilbakestille passordet er sendt viss du har ei registrert e-postadresse hos oss. Du vil bli omdirigert til innloggingssida.'
+        );
         startTransition(async () => {
             await forgotPassword(email);
-            router.replace('/login');
+            await new Promise((resolve) => {
+                setTimeout(async () => {
+                    resolve(router.replace('/login'));
+                }, 4000);
+            });
         });
     };
     return (
@@ -32,6 +40,7 @@ const ForgotPassword = () => {
                 sx={{ placeContent: 'center', height: '100%', flexDirection: 'column', gap: '1rem' }}
                 onSubmit={handleSubmit}
             >
+                <Typography>{message}</Typography>
                 <EmailTextField defaultValue={searchParamEmail ?? undefined} />
                 <Button
                     type="submit"
