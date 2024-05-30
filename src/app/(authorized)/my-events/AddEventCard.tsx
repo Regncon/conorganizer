e8528d3c988ea86387e08800a457c4e099091b8f
CoLdAs1cE <coldasice_t@hotmail.com>
2024-05-30 19:08:11 +0200

    Fix: replace FloatingActionButton with an AddEventCard and some small styling fixes

--- src/app/(authorized)/my-events/AddEventCard.tsx
+++ src/app/(authorized)/my-events/AddEventCard.tsx
@@ -19,46 +19,40 @@ const AddEventCard = ({ newDocumentId }: Props) => {
         router.push(`/event/create/${newDocumentId}`);
     };
     return (
-        <Box
-            onClick={() => {
-                console.log('clicked box 4 more box more click');
+        <Card
+            sx={{
+                minHeight: '267px',
+                minWidth: '306px',
+                maxHeight: '267px',
+                maxWidth: '306px',
+                height: '100%',
+                width: '100%',
+                backgroundSize: 'cover',
+                borderRadius: '1.75rem',
+                opacity: 0.7,
             }}
         >
-            <Card
-                sx={{
-                    minHeight: '267px',
-                    minWidth: '306px',
-                    maxHeight: '267px',
-                    maxWidth: '306px',
-                    height: '100%',
-                    width: '100%',
-                    backgroundSize: 'cover',
-                    borderRadius: '1.75rem',
-                    opacity: 0.7,
-                }}
-            >
-                <CardActionArea sx={{}}>
-                    <Box
-                        sx={{
-                            display: 'block',
-                            minHeight: '267px',
-                            minWidth: '306px',
-                            maxHeight: '267px',
-                            maxWidth: '306px',
-                            height: '100%',
-                            width: '100%',
-                        }}
-                    >
-                        <Typography variant="h3" sx={{ textAlign: 'center' }}>
-                            Legg til nytt arrangement
-                        </Typography>
-                        <Box sx={{ display: 'flex', justifyContent:'center' }}>
-                            <AddIcon sx={{ fontSize: '10rem' }} />
-                        </Box>
+            <CardActionArea onClick={handleClick}>
+                <Box
+                    sx={{
+                        display: 'block',
+                        minHeight: '267px',
+                        minWidth: '306px',
+                        maxHeight: '267px',
+                        maxWidth: '306px',
+                        height: '100%',
+                        width: '100%',
+                    }}
+                >
+                    <Typography variant="h3" sx={{ textAlign: 'center' }}>
+                        Legg til nytt arrangement
+                    </Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', paddingBlockStart: '1.5rem' }}>
+                        <AddIcon sx={{ fontSize: '10rem' }} />
                     </Box>
-                </CardActionArea>
-            </Card>
-        </Box>
+                </Box>
+            </CardActionArea>
+        </Card>
     );
 };
 export default AddEventCard;
--- src/app/(authorized)/my-events/page.tsx
+++ src/app/(authorized)/my-events/page.tsx
@@ -5,11 +5,10 @@ import type { FirebaseApp } from 'firebase/app';
 import Box from '@mui/material/Box';
 import { getAllMyEvents } from './actions';
 import RealtimeMyEvents from './RealtimeMyEvents';
-import { Paper, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { revalidatePath } from 'next/cache';
 import EventCardBig from '$app/(public)/EventCardBig';
 import DynamicLink from './DynamicLink';
-import FloatingActionButton from './FloatingActionButton';
 import AddEventCard from './AddEventCard';
 const createId = async (app: FirebaseApp, db: Firestore) => {
     const collectionRef = collection(db, '_');
@@ -65,9 +64,17 @@ const MyEvents = async () => {
                                 </DynamicLink>
                             </Grid2>
                         ))}
-                    <AddEventCard newDocumentId={newDocumentId} />
+                    <Grid2
+                        sx={{
+                            display: 'flex',
+                            placeContent: 'center',
+                            placeItems: 'center',
+                        }}
+                        xl={2.7}
+                    >
+                        <AddEventCard newDocumentId={newDocumentId} />
+                    </Grid2>
                 </Grid2>
-                <FloatingActionButton newDocumentId={newDocumentId} />
                 <RealtimeMyEvents userId={user.uid} />
             </Box>
         </>
