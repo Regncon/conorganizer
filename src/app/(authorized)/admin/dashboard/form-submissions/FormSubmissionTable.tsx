0066967249c55a6649629575ce7c0311d2963115
CoLdAs1cE <coldasice_t@hotmail.com>
2024-06-16 16:49:02 +0200

    Cleaned up file, added prefetching when hovering row, go to preview table action, add autoheight prop and removed checkboxselection prop

--- src/app/(authorized)/admin/dashboard/form-submissions/FormSubmissionTable.tsx
+++ src/app/(authorized)/admin/dashboard/form-submissions/FormSubmissionTable.tsx
@@ -1,124 +1,31 @@
 'use client';
-import { DataGrid, useGridApiRef, type GridComparatorFn, type GridRowParams } from '@mui/x-data-grid';
-import type { FormSubmission, FormSubmissionColDef } from './types';
-import { useRealtimeTableData } from './hooks';
-import { useMemo, useRef } from 'react';
-import { updateReadStatus } from './actions';
+import { DataGrid, useGridApiRef, type GridEventListener, type GridRowParams } from '@mui/x-data-grid';
+import type { FormSubmission } from './types';
+import { useColumns } from './hooks/useColumns';
+import { useRealtimeTableData } from './hooks/useRealtimeTableData';
+import { useEffect, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
 import { Route } from 'next';
 
-const columns: FormSubmissionColDef = [
-    // { field: 'id', headerName: 'ID', width: 255 },
-    {
-        field: 'name',
-        headerName: 'Navn',
-        width: 140,
-        editable: true,
-    },
-    {
-        field: 'title',
-        headerName: 'Tittel',
-        width: 140,
-        editable: true,
-    },
-    {
-        field: 'subTitle',
-        headerName: 'Kort beskrivelse',
-        width: 240,
-        editable: true,
-    },
-    {
-        field: 'isRead',
-        headerName: 'Lest',
-        type: 'boolean',
-        width: 110,
-        editable: true,
-    },
-    {
-        field: 'isAccepted',
-        headerName: 'Godkjent',
-        type: 'boolean',
-        width: 110,
-        editable: true,
-    },
-];
-
-// const rows: FormSubmission[] = [
-//     {
-//         id: '377104b0-7354-5125-81c1-167ddce1df1e',
-//         title: 'further',
-//         subTitle: 'stay elephant wore rocky him cattle describe shelf itself activity ',
-//         isRead: true,
-//         isAccepted: false,
-//     },
-//     {
-//         id: '2dc4d222-a50c-5ddd-b607-7df06abcd36e',
-//         title: 'saw',
-//         subTitle: 'horse imagine saw courage way call ask brush has tongue ',
-//         isRead: true,
-//         isAccepted: false,
-//     },
-//     {
-//         id: 'd6bf7fb7-9b0e-5075-aa94-5cc1c312ec31',
-//         title: 'nearest',
-//         subTitle: 'dish observe total shells leg sharp elephant put whole report ',
-//         isRead: true,
-//         isAccepted: false,
-//     },
-//     {
-//         id: '5932bf9a-ddff-5b9a-99dc-561240fbb8b8',
-//         title: 'way',
-//         subTitle: 'score beautiful roar danger nine expression far twenty rise stronger ',
-//         isRead: true,
-//         isAccepted: false,
-//     },
-//     {
-//         id: '39531de4-08c5-5ccb-99eb-a45228e3f063',
-//         title: 'climb',
-//         subTitle: 'reach hurried successful dirty egg fall victory cell track although ',
-//         isRead: true,
-//         isAccepted: false,
-//     },
-//     {
-//         id: '191d972a-20aa-52b5-a359-1546ec517973',
-//         title: 'title',
-//         subTitle: 'shelf very generally invented pie promised type gift is outside ',
-//         isRead: true,
-//         isAccepted: false,
-//     },
-//     {
-//         id: '3bf5a011-c2eb-5e45-9193-896cb4060af9',
-//         title: 'slope',
-//         subTitle: 'arrow feed facing putting many telephone ants pale wire more ',
-//         isRead: true,
-//         isAccepted: false,
-//     },
-//     {
-//         id: '6f0c5d97-e683-5c21-b9f2-123b70ab4c18',
-//         title: 'note',
-//         subTitle: 'below glass engine fence sell were town passage sister spent ',
-//         isRead: true,
-//         isAccepted: false,
-//     },
-//     {
-//         id: '67dfe3c9-62d0-5378-aba0-ddc6a33bc46f',
-//         title: 'same',
-//         subTitle: 'kids took powerful national live football memory has select doing ',
-//         isRead: true,
-//         isAccepted: false,
-//     },
-// ];
-
 const FormSubmissionTable = () => {
+    const columns = useColumns();
     const tableData = useRealtimeTableData();
     const apiRef = useGridApiRef();
     const rows = useMemo(() => tableData, [tableData]);
     const router = useRouter();
 
+    useEffect(() => {
+        const handleRowHover: GridEventListener<'rowMouseEnter'> = (params: GridRowParams<FormSubmission>) => {
+            router.prefetch(`/admin/dashboard/form-submissions/preview/${params.id}` as Route);
+        };
+
+        return apiRef.current.subscribeEvent('rowMouseEnter', handleRowHover);
+    }, [apiRef]);
+
     return (
         <DataGrid
-            sx={{ minHeight: '40rem', insetBlockStart: '2rem' }}
-            rows={rows}
+            sx={{ insetBlockStart: '2rem' }}
+            rows={rows ? rows : []}
             columns={columns}
             initialState={{
                 pagination: {
@@ -135,23 +42,15 @@ const FormSubmissionTable = () => {
                     ],
                 },
             }}
-            // onRowSelectionModelChange={(e, b) => {
-            //     console.log(e, 'e', b.api.getRow(b.api.getAllRowIds()[0]), 'b');
-            // }}
-            onRowClick={(e: GridRowParams<FormSubmission>) => {
-                console.log(e.row);
-                updateReadStatus(e.row.id);
-                router.push(`/admin/dashboard/form-submissions/preview/${e.row.id}` as Route);
-                //Todo: Få Torstein til å forklare dette på nytt.
-            }}
             pageSizeOptions={[10]}
             onPaginationModelChange={(e) => {
                 console.log(e);
             }}
-            loading={rows.length === 0}
-            checkboxSelection
+            loading={!!!rows}
+            // checkboxSelection
             disableRowSelectionOnClick
             apiRef={apiRef}
+            autoHeight
         />
     );
 };
