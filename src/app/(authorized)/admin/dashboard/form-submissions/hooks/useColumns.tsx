1cf97e98d7727873dceebb99481bcc027a5c97c2
CoLdAs1cE <coldasice_t@hotmail.com>
2024-06-16 16:46:56 +0200

    seperated out columns to a hook since to use hooks in it

---
+++ src/app/(authorized)/admin/dashboard/form-submissions/hooks/useColumns.tsx
@@ -0,0 +1,66 @@
+import { useMemo } from 'react';
+import type { FormSubmissionColDef } from '../types';
+import { useRouter } from 'next/navigation';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { GridActionsCellItem } from '@mui/x-data-grid';
+import type { Route } from 'next';
+
+export const useColumns = () => {
+    const router = useRouter();
+    const columns: FormSubmissionColDef = useMemo(
+        () => [
+            // { field: 'id', headerName: 'ID', width: 255,  },
+            {
+                field: 'name',
+                headerName: 'Navn',
+                width: 140,
+                editable: true,
+            },
+            {
+                field: 'title',
+                headerName: 'Tittel',
+                width: 140,
+                editable: true,
+            },
+            {
+                field: 'subTitle',
+                headerName: 'Kort beskrivelse',
+                width: 240,
+                editable: true,
+            },
+            {
+                field: 'isRead',
+                headerName: 'Lest',
+                type: 'boolean',
+                width: 110,
+                editable: true,
+            },
+            {
+                field: 'isAccepted',
+                headerName: 'Godkjent',
+                type: 'boolean',
+                width: 120,
+                editable: true,
+            },
+            {
+                field: 'actions',
+                type: 'actions',
+                headerName: 'Gå til førehandsvising',
+                width: 150,
+                getActions: (params) => [
+                    <GridActionsCellItem
+                        icon={<FontAwesomeIcon icon={faArrowUpRightFromSquare} />}
+                        label="førehandsvising"
+                        onClick={(e) => {
+                            router.push(`/admin/dashboard/form-submissions/preview/${params.id}` as Route);
+                        }}
+                    />,
+                ],
+            },
+        ],
+        [router]
+    );
+
+    return columns;
+};
