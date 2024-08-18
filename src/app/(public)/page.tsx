0ce0a253541c41f514411231483c0c53cc1eecbe
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2024-08-18 16:36:27 +0200

    Added new logo.

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,57 +3,45 @@ import EventCardSmall from './EventCardSmall';
 import { getAllEvents } from './serverAction';
 import RealtimeEvents from './RealtimeEvents';
 import Grid from '@mui/material/Unstable_Grid2';
-import { redirect } from 'next/navigation';
-import { redirectToAdminDashboardWhenAdministrator } from '$lib/lib';
+import Image from 'next/image';
+import { Box } from '@mui/material';
 
 export default async function Home() {
-    // await redirectToAdminDashboardWhenAdministrator();
-
-    // redirect('/dashboard');
     const events = await getAllEvents();
 
     return (
-        <>
-            <img src="/placeholderlogo.png" alt="logo" />
+        <Box>
+            <img src="/RegnCon2024LogoWhite.webp" style={{ width: '100%', objectFit: 'scale-down' }} alt="logo" />
             <Grid container spacing={2}>
-                {events
-                    .filter((ce) => ce.published)
-                    .map((event, i) => {
-                        return (
-                            <Grid xs={i === 0 ? 12 : 6}>
-                                {i === 0 ?
-                                    <Grid xs={12}>
-                                        <EventCardBig
+                <Grid container spacing={2}>
+                    {events
+                        .filter((ce) => ce.published)
+                        .map((event, i) => {
+                            return (
+                                <Grid xs={i === 0 ? 12 : 6}>
+                                    {i === 0 ?
+                                        <Grid xs={12}>
+                                            <EventCardBig
+                                                key={i}
+                                                title={event.title}
+                                                gameMaster={event.gameMaster}
+                                                shortDescription={event.shortDescription}
+                                                system={event.system}
+                                            />
+                                        </Grid>
+                                        : <EventCardSmall
                                             key={i}
                                             title={event.title}
                                             gameMaster={event.gameMaster}
-                                            shortDescription={event.shortDescription}
                                             system={event.system}
                                         />
-                                    </Grid>
-                                    : <EventCardSmall
-                                        key={i}
-                                        title={event.title}
-                                        gameMaster={event.gameMaster}
-                                        system={event.system}
-                                    />
-                                }
-                            </Grid>
-                        );
-                    })}
+                                    }
+                                </Grid>
+                            );
+                        })}
+                </Grid>
+                <RealtimeEvents />
             </Grid>
-            {/* <EventCardBig
-    					title="Hello world"
-    					gameMaster="Gerhard Fajita"
-    					shortDescription="Mord overalt! Kos! Gøy!"
-    					system="Call of Chthuhlth"
-    				/>
-    				<Box sx={{ display: 'flex' }}>
-    					<EventCardSmall title="Hi" gameMaster="Gardh Fajita2" system="Dungeons 2" />
-    					<EventCardSmall title="Any% speedrun" gameMaster="Gorde Fajita3" system="Terraria" />
-    				</Box> */}
-
-            <RealtimeEvents />
-        </>
+        </Box>
     );
 }
