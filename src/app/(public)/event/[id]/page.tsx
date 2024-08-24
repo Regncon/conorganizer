bf2351dfc2c9677adb29ff79955ee9831d9a9d4e
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2024-08-24 17:54:11 +0200

    Set page title on event

--- src/app/(public)/event/[id]/page.tsx
+++ src/app/(public)/event/[id]/page.tsx
@@ -1,8 +1,19 @@
+import { getEventById } from '$app/(public)/serverAction';
+import { Metadata } from 'next';
 import MainEvent from './event';
 
 type Props = {
     params: { id: string };
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const id = params.id;
+    const event = await getEventById(id);
+    return {
+        title: event.title,
+        description: event.shortDescription,
+    };
+}
 
 const EventPage = ({ params: { id } }: Props) => {
     return <MainEvent id={id} />;
--- src/app/(public)/serverAction.ts
+++ src/app/(public)/serverAction.ts
@@ -2,13 +2,17 @@
 import { adminDb } from '$lib/firebase/firebaseAdmin';
 import { revalidatePath } from 'next/cache';
 import type { ConEvent } from '../../lib/types';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { firebaseAuth } from '$lib/firebase/firebase';
 
 export async function getAllEvents() {
     const eventRef = await adminDb.collection('events').get();
     const events = eventRef.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as ConEvent[];
     return events;
+}
+
+export async function getEventById(id: string) {
+    const event = (await adminDb.collection('events').doc(id).get()).data() as ConEvent;
+    return { ...event, id};
 }
 
 export async function updateEvents() {
