8914a5279443eb1f43fc05d5311ab34042f1a1b0
CoLdAs1cE <coldasice_t@hotmail.com>
2024-09-01 15:50:07 +0200

    Disaster should be fixed :D

---
+++ src/app/(authorized)/dashboard/MyTickets.tsx
@@ -0,0 +1,15 @@
+import CardBase from './CardBase';
+
+const MyTickets = () => {
+    return (
+        <CardBase
+            href="/my-profile/my-tickets"
+            subTitle="Trykk for og gå til mine billetter"
+            img="/my-tickets.jpg"
+            imgAlt="Mine billeter"
+            title="Mine billetter"
+        />
+    );
+};
+
+export default MyTickets;
--- src/app/(authorized)/my-profile/page.tsx
+++ src/app/(authorized)/my-profile/page.tsx
@@ -1,6 +1,8 @@
 import { getAuthorizedAuth } from '$lib/firebase/firebaseAdmin';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Paper, Typography } from '@mui/material';
 import { GetTicketsByEmail, GetTicketsFromCheckIn } from './my-tickets/actions';
+import MyEvents from '../dashboard/MyEvents';
+import MyTickets from '../dashboard/MyTickets';
 
 export default async function MyProfile() {
     const { user } = await getAuthorizedAuth();
@@ -15,13 +17,20 @@ export default async function MyProfile() {
     //console.log(JSON.stringify(await GetTicketsFromCheckIn()), 'GetTicketsFromCheckIn');
 
     return (
-        <Box>
-            <Button> Do some magic</Button>
+        <Paper>
             <Typography variant="h1">My Profile</Typography>
             <Typography variant="body1">
                 Hello world! There'll be stuff here at some point, but bear with me for now. Anyways, how's your day? I
-                hope you're doing well.{' '}
+                hope you're doing well.
             </Typography>
-        </Box>
+            <Typography variant="body1"> Anyways, here are the events you sent in. </Typography>
+            <MyEvents />
+            <Typography variant="body1"> And here are your tickets. I think? </Typography>
+            <MyTickets />
+            <Typography variant="body1"> Is there something wrong? </Typography>
+            <Button variant="contained" color="primary" href="/">
+                I'm not supposed to be here!
+            </Button>
+        </Paper>
     );
 }
