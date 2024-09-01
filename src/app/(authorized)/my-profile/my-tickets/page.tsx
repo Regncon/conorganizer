3723132af73bac033fd829b1866ddd0f5e9e8534
CoLdAs1cE <coldasice_t@hotmail.com>
2024-09-01 02:57:35 +0200

    Refactor code and refactor to new folder and components & implimented confrim email button and use signin with gmail button instead and add link to buy ticket button

--- src/app/(authorized)/my-profile/my-tickets/page.tsx
+++ src/app/(authorized)/my-profile/my-tickets/page.tsx
@@ -1,34 +1,20 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
-import Ticket from "./Ticket";
-import CustomIcon from "$lib/icons/miscgame";
+import { Box, Button } from '@mui/material';
+import ConfirmOrBuy from './MyTicketsComponents/ConfirmOrBuy';
+import TicketNotFound from './MyTicketsComponents/TicketNotFound';
+import Tickets from './MyTicketsComponents/Tickets';
 
-export default function MyTickets() {
+const MyTickets = async () => {
     return (
-        <Box sx={{display:"flex", paddingLeft:"2rem", gap:"1rem"}}>
-            <Paper sx={{marginBottom: "2rem", paddingLeft:"2rem", maxWidth:"320px"}}>
-                <Typography variant="h1">Bekreft e-post/Mine billetter</Typography>
-                <Typography variant="h2">Har billetter</Typography>
-                <Button variant="contained" color="primary">Bekreft e-post</Button>
-                <Button variant="contained" color="primary">Log inn med Google</Button>
-                <Typography variant="h2">Har ikke billetter</Typography>
-                <Button variant="contained" color="primary">Kjøp billett</Button>
-            </Paper>
-            <Paper sx={{marginBottom: "2rem", paddingLeft:"2rem", maxWidth:"320px"}}>
-                <Typography variant="h1">Ingen?/Mine billetter</Typography>
-                <Typography variant="h2">Fant ingen billetter.</Typography>
-                <Button variant="contained" color="primary">Kjøp billett</Button>
-                <Button variant="contained" color="primary">Har allerede kjøpt billett</Button>
-                <TextField label="Skriv inn e-posten du brukte på Checkin" />
-                <Button variant="contained" color="primary">Hent billett</Button>
-            </Paper>
-            <Paper sx={{marginBottom: "2rem", paddingLeft:"2rem", width:"320px"}}>
-                <Typography variant="h1">My Tickets</Typography>
-                <CustomIcon color="primary" size="large" />
-                <CustomIcon color="secondary" size="small" />
-                <Ticket /> 
-                <Ticket />
-            </Paper>
-        <Button variant="contained" color="primary" href="/my-profile">Go back to my profile</Button>
+        <Box sx={{ display: 'flex', paddingLeft: '2rem', gap: '1rem' }}>
+            <ConfirmOrBuy />
+            <TicketNotFound />
+            <Tickets />
+
+            <Button variant="contained" color="primary" href="/my-profile">
+                Go back to my profile
+            </Button>
         </Box>
     );
-}
+};
+
+export default MyTickets;
