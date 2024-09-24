942dd2b6787ae2ff9ac194f62063c5ef04a5c671
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2024-09-24 14:00:20 +0200

    test for tickets belonging to user refactor.

--- src/app/(authorized)/my-profile/my-tickets/components/lib/actions/NewTickets.ts
+++ src/app/(authorized)/my-profile/my-tickets/components/lib/actions/NewTickets.ts
@@ -7,16 +7,23 @@ export const NewTickets = async (tickets: EventTicket[], participants: Participa
     console.log('Assigning new tickets to participants');
     // console.log('tickes', tickets, 'participants', participants, 'user', user);
 
-    const ticketsWithUsersEmail = tickets?.filter((ticket) => ticket.crm.email === (user.email as string));
-    const ticketsWithOrderNumberFromEmail = tickets?.filter((ticket) =>
-        ticketsWithUsersEmail?.some((emailTicket) => emailTicket.order_id === ticket.order_id)
-    );
+    const usersTickets = GetTicketsThatBelongToUser(tickets, user);
 
     const newParticipants: Participant[] = [];
-    ticketsWithOrderNumberFromEmail.forEach((ticket) => {
+    usersTickets.forEach((ticket) => {
         const newParticipant = generateParticipant(ticket.id, tickets, user.email as string);
         newParticipant.users = [user.uid];
         newParticipants.push(newParticipant);
     });
     return newParticipants;
+};
+
+const GetTicketsThatBelongToUser = (tickets: EventTicket[], user: User) => {
+    const ticketsWithUsersEmail = tickets?.filter((ticket) => ticket.crm.email === (user.email as string));
+
+    const ticketsWithOrderNumberFromEmail = tickets?.filter((ticket) =>
+        ticketsWithUsersEmail?.some((emailTicket) => emailTicket.order_id === ticket.order_id)
+    );
+
+    return ticketsWithOrderNumberFromEmail;
 };
