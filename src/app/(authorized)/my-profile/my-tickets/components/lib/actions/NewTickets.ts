dc021be1934ed43bf86d8fa8b20ba652538bd7a7
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2024-09-24 13:54:52 +0200

    test for only include tickets that belongs to user green

--- src/app/(authorized)/my-profile/my-tickets/components/lib/actions/NewTickets.ts
+++ src/app/(authorized)/my-profile/my-tickets/components/lib/actions/NewTickets.ts
@@ -7,8 +7,13 @@ export const NewTickets = async (tickets: EventTicket[], participants: Participa
     console.log('Assigning new tickets to participants');
     // console.log('tickes', tickets, 'participants', participants, 'user', user);
 
+    const ticketsWithUsersEmail = tickets?.filter((ticket) => ticket.crm.email === (user.email as string));
+    const ticketsWithOrderNumberFromEmail = tickets?.filter((ticket) =>
+        ticketsWithUsersEmail?.some((emailTicket) => emailTicket.order_id === ticket.order_id)
+    );
+
     const newParticipants: Participant[] = [];
-    tickets.forEach((ticket) => {
+    ticketsWithOrderNumberFromEmail.forEach((ticket) => {
         const newParticipant = generateParticipant(ticket.id, tickets, user.email as string);
         newParticipant.users = [user.uid];
         newParticipants.push(newParticipant);
