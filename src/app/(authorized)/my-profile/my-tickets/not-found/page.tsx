e27f6e6da12b16422ae9e0856d98308cbd2b4e02
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2024-09-25 21:30:47 +0200

    Added help texts for tickets.

--- src/app/(authorized)/my-profile/my-tickets/not-found/page.tsx
+++ src/app/(authorized)/my-profile/my-tickets/not-found/page.tsx
@@ -1,4 +1,4 @@
-import { Paper, Typography, Box } from '@mui/material';
+import { Paper, Typography, Box, Link } from '@mui/material';
 import BuyTicketButton from '../shared/ui/BuyTicketButton';
 
 type Props = {};
@@ -9,12 +9,20 @@ const TicketNotFound = ({ }: Props) => {
             <Paper sx={{ marginBottom: '2rem', paddingInline: '1rem', maxWidth: '400px' }}>
                 <Typography variant="h1">Fant ingen billetter.</Typography>
                 <Typography>
-                    Legge inn en fin hjelpetekst skrevet av en som ikke er meg eller dyslektiker som forklarer at folk
-                    må ta kontakt dersom de trenger hjelp
+                    Vi fann ingen billettar registrert på denne epostadressa. Det betyr at du anten ikkje har kjøpt
+                    billettar endå, eller at du har kjøpt billettane på ei anna anna epostadresse enn den du er logga
+                    inn med her.
                 </Typography>
-                <Box sx={{ display: 'grid', gap: '1rem', marginBlockEnd: '1rem' }}>
+                <Box sx={{ display: 'grid', gap: '1rem', marginBlock: '2rem' }}>
                     <BuyTicketButton />
                 </Box>
+                <Typography sx={{ marginBottom: '1rem' }}>
+                    Kjøp billettar på Checkin, lag ein brukar på riktig mailadresse, eller ta kontakt med
+                    <Typography component="span" sx={{ color: 'primary.main', marginInline: '1ch' }}>
+                        <Link href={'mailto:styret@regncon.no'}>styret@regncon.no</Link>
+                    </Typography>
+                    noko er galt, eller om du ønsker billettane overført til epostadressa du har laga brukar til.
+                </Typography>
             </Paper>
         </Box>
     );
