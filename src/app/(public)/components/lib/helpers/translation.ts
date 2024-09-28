5564390e5f1f342e1b5ce7e607cc63127541397b
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2024-09-28 11:19:00 +0200

    Added time to day separator.

--- src/app/(public)/components/lib/helpers/translation.ts
+++ src/app/(public)/components/lib/helpers/translation.ts
@@ -8,3 +8,12 @@ export const translatedDays = new Map<PoolName, string>([
 ]);
 
 export const getTranslatedDay = (day: PoolName) => translatedDays.get(day);
+
+export const translatedDaysAndTime = new Map<PoolName, string>([
+    [PoolName.fridayEvening, 'Fredag kveld Kl 18 - 23'],
+    [PoolName.saturdayMorning, 'Lørdag morgen Kl 10 - 15'],
+    [PoolName.saturdayEvening, 'Lørdag kveld Kl 18 - 23'],
+    [PoolName.sundayMorning, 'Søndag morgen Kl 10 - 15'],
+]);
+
+export const getTranslatedDayAndTime = (day: PoolName) => translatedDaysAndTime.get(day);
