667a8f3289357fefbb2fd76d81610e9999711933
CoLdAs1cE <coldasice_t@hotmail.com>
2024-10-01 23:12:49 +0200

    Migrated all Poolnames missing in Participant Interest and PoolEvent Interests missing connection between participant and user to make interest page work again

--- src/lib/serverActions/Migration.ts
+++ src/lib/serverActions/Migration.ts
@@ -30,60 +30,163 @@ export const migrateParticipantAndInterest = async () => {
         return undefined;
     }) as Interest[];
 
-    const test = GetAllInterests.filter(Boolean).map((interest) => {
-        return adminDb
-            .collection('participants')
-            .doc(interest.participantId)
-            .collection('interests')
-            .doc(interest.id ?? '')
-            .get()
-            .then((res) => res.ref.path);
-    });
-
-    //missing poolName
+    // ----------------------------------------------------------------
+    //missing poolName in interests
+    // ----------------------------------------------------------------
     // console.log(
-    //     'GetAllParticipants',
-    //     GetAllInterests.filter((participant) => participant.poolName === undefined)
+    //     'GetAllInterests',
+    //     GetAllInterests.filter((interest) => interest.poolName === undefined)
     // );
     // console.log(
-    //     'GetAllParticipants2',
-    //     GetAllInterests2.filter((participant) => participant.poolName === undefined)
+    //     'GetAllInterests2',
+    //     GetAllInterests2.filter((interest) => interest.poolName === undefined)
     // );
 
     // // missing firstname
     // console.log(
-    //     'GetAllParticipants',
-    //     GetAllInterests.filter((participant) => participant.participantFirstName === undefined)
+    //     'GetAllInterests',
+    //     GetAllInterests.filter((interest) => interest.participantFirstName === undefined)
     // );
     // console.log(
-    //     'GetAllParticipants2',
-    //     GetAllInterests2.filter((participant) => participant.participantFirstName === undefined)
+    //     'GetAllInterests2',
+    //     GetAllInterests2.filter((interest) => interest.participantFirstName === undefined)
     // );
-    const test2 = await Promise.all(test);
-    console.log(GetAllInterests.length, 'legnth');
 
-    const onlyWrongNameProperty = GetAllInterests.filter(Boolean).filter(
-        (participant) => participant.pardicipantFirstName !== undefined
-    );
-    const onlyWrongNameProperty2 = GetAllInterests2.filter(Boolean).filter(
-        (participant) => participant.pardicipantFirstName !== undefined
-    );
+    // const interestOnlyWrongNameProperty = GetAllInterests.filter(Boolean).filter(
+    //     (interest) => interest.pardicipantFirstName !== undefined
+    // );
+    // const interestOnlyWrongNameProperty2 = GetAllInterests2.filter(Boolean).filter(
+    //     (interest) => interest.pardicipantFirstName !== undefined
+    // );
 
-    // const updateThis = await adminDb
+    // fix name issues in interests
+    // const interestOnlyWrongNamePropertyUpdateDb = interestOnlyWrongNameProperty.map((interest) => {
+    //     return adminDb
+    //         .collection('participants')
+    //         .doc(interest.participantId)
+    //         .collection('interests')
+    //         .doc(interest.id ?? '')
+    //         .update({ participantFirstName: interest.pardicipantFirstName });
+    // });
+
+    // const interestOnlyWrongNameProperty2UpdateDb = interestOnlyWrongNameProperty2.map((interest) => {
+    //     return adminDb
+    //         .collection('participants')
+    //         .doc(interest.participantId)
+    //         .collection('participant-interests')
+    //         .doc(interest.id ?? '')
+    //         .update({ participantFirstName: interest.pardicipantFirstName });
+    // });
+
+    // return {
+    //     0: interestOnlyWrongNameProperty,
+    //     1: interestOnlyWrongNameProperty2,
+    // };
+
+    // ----------------------------------------------------------------
+    // migrate inn poolnames to intrests (from 'interest')
+    // ----------------------------------------------------------------
+
+    // const interestOnlyWrongNameProperty = GetAllInterests.filter(Boolean).filter(
+    //     (interest) => interest.poolName === undefined
+    // );
+
+    // const allPoolEvents = (await adminDb.collection('pool-events').get()).docs.map((doc) => ({
+    //     id: doc.id,
+    //     ...doc.data(),
+    // })) as PoolEvent[];
+
+    // const allPoolEventsWithInterestsPoolNamesThatArMissing = allPoolEvents.filter((poolEvent) =>
+    //     interestOnlyWrongNameProperty.some((interest) => interest.poolEventId === poolEvent.id)
+    // );
+    // const interestOnlyWrongNamePropertyUpdateDb = interestOnlyWrongNameProperty.map((interest) => {
+    //     const poolEvent = allPoolEvents.find((poolEvent) => poolEvent.id === interest.poolEventId);
+    //     if (poolEvent === undefined) {
+    //         return;
+    //     }
+    //     return adminDb
+    //         .collection('participants')
+    //         .doc(interest.participantId)
+    //         .collection('interests')
+    //         .doc(interest.id ?? '')
+    //         .update({ poolName: poolEvent.poolName, updatedBy: 'migrate interests and poolNames' });
+    // });
+
+    // const updateResult = await Promise.all(interestOnlyWrongNamePropertyUpdateDb);
+    // console.log(updateResult);
+
+    // const updateTest = await adminDb
     //     .collection('participants')
-    //     .doc('FqxDY3CjlGfLqW5PlfSA')
+    //     .doc('0bNx792Iz1qk2eKS5OZx')
     //     .collection('interests')
-    //     .doc('5IaJJcmNxarqkcnjqlcl')
-    //     .update({ participantFirstName: 'test' });
-    // console.log(updateThis);
+    //     .doc('MnmNgvVQuay4S5B3mMLG')
+    //     // .update({ poolName: 'fridayEvening' });
+    //     .get()
+    //     .then((doc) => doc.data());
+    // console.log(updateTest);
+    // return {
+    //     0: interestOnlyWrongNameProperty,
+    //     // 1: interestOnlyWrongNameProperty2,
+    //     2: allPoolEvents,
+    //     3: allPoolEventsWithInterestsPoolNamesThatArMissing,
+    // };
 
-    return {
-        0: onlyWrongNameProperty,
-        1: onlyWrongNameProperty2,
-        2: test2,
-    };
+    // ----------------------------------------------------------------
+    // migrate in poolName from 'poolEvents' to 'interests in pool-event'
+    // ----------------------------------------------------------------
 
-    // migrate inn poolnames to intrests (from 'interest' and 'participant-interests')
+    // const GetAllPoolEventsInterests = (await adminDb.collectionGroup('interests').get()).docs.map((doc) => {
+    //     if (doc.ref.path.includes('pool-events')) {
+    //         return {
+    //             id: doc.id,
+    //             ...doc.data(),
+    //         };
+    //     }
+    //     return undefined;
+    // }) as Interest[];
+    // const poolEventInterestOnlyWrongNameProperty = GetAllPoolEventsInterests.filter(Boolean).filter(
+    //     (interest) => interest.poolName === undefined
+    // );
 
+    // const allPoolEvents = (await adminDb.collection('pool-events').get()).docs.map((doc) => ({
+    //     id: doc.id,
+    //     ...doc.data(),
+    // })) as PoolEvent[];
+
+    // const allPoolEventsWithPoolEventsInterestsPoolNamesThatArMissing = allPoolEvents.filter((poolEvent) =>
+    //     poolEventInterestOnlyWrongNameProperty.some((interest) => interest.poolEventId === poolEvent.id)
+    // );
+    // const interestOnlyWrongNamePropertyUpdateDb = poolEventInterestOnlyWrongNameProperty.map((interest) => {
+    //     const poolEvent = allPoolEvents.find((poolEvent) => poolEvent.id === interest.poolEventId);
+    //     if (poolEvent === undefined) {
+    //         return;
+    //     }
+    //     return adminDb
+    //         .collection('pool-events')
+    //         .doc(interest.poolEventId)
+    //         .collection('interests')
+    //         .doc(interest.id ?? '')
+    //         .update({ poolName: poolEvent.poolName, updatedBy: 'migrate interests and poolNames' });
+    // });
+
+    // const updateResult = await Promise.all(interestOnlyWrongNamePropertyUpdateDb);
+    // console.log(updateResult);
+
+    // const updateTest = await adminDb
+    //     .collection('pool-events')
+    //     .doc('qZmV7evJ749YjyAiRKMe')
+    //     .collection('interests')
+    //     .doc('BhTrf9kH5nzy4rxbV4ru')
+    //     // .update({ poolName: 'fridayEvening', updatedBy: 'migrate interests and poolNames' });
+    // .get()
+    // .then((doc) => doc.data());
+    // console.log(updateTest);
+
+    // return {
+    //     0: poolEventInterestOnlyWrongNameProperty,
+    //     // 1: interestOnlyWrongNameProperty2,
+    //     2: allPoolEvents,
+    //     3: allPoolEventsWithPoolEventsInterestsPoolNamesThatArMissing,
+    // };
     // migrate in name from pardicipantFirstName to participantFirstName  (from 'interest' and 'participant-interests')
 };
