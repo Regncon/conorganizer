14408309efc1f522a8d7dded2ae77138f8c8b099
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2024-10-02 21:14:14 +0200

    pool interest get participant

--- src/app/(authorized)/admin/dashboard/events/event-dashboard/[id]/[tab]/players/components/lib/actions.ts
+++ src/app/(authorized)/admin/dashboard/events/event-dashboard/[id]/[tab]/players/components/lib/actions.ts
@@ -1,22 +1,30 @@
-import { adminDb } from '$lib/firebase/firebaseAdmin';
-import { ConEvent, Interest, PlayerInterest } from '$lib/types';
+import { adminDb, getAuthorizedAuth } from '$lib/firebase/firebaseAdmin';
+import { ConEvent, Interest, Participant, PlayerInterest } from '$lib/types';
 
 export async function generatePoolPlayerInterestById(id: string) {
+    const { db, user } = await getAuthorizedAuth();
+
     let poolInterests: PlayerInterest[] = [];
 
     const interestRef = await adminDb.collection('pool-events').doc(id).collection('interests').get();
     const interests = interestRef.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as Interest[];
 
+    const participants = (await adminDb.collection('participants').get()).docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    })) as Participant[];
+
     interests.forEach((interest) => {
+        const participant = participants.find((participant) => participant.id === interest.participantId);
         const playerInterest: PlayerInterest = {
             poolEventId: interest.poolEventId,
             interestLevel: interest.interestLevel,
             participantId: interest.participantId,
             firstName: interest.participantFirstName,
             lastName: interest.participantLastName,
-            isOver18: false,
-            ticketCategoryID: 0,
-            ticketCategory: '',
+            isOver18: participant?.over18 ?? false,
+            ticketCategoryID: participant?.ticketCategoryId ?? 0,
+            ticketCategory: participant?.ticketCategory ?? 'FEIL!',
             conPlayers: [],
         };
 
