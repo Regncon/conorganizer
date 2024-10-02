1159adc943d3fdc0b029f501b2f04ad842c177aa
Gerhard Just-Olsen <gerhard.olsen@tidsbanken.no>
2024-10-02 21:24:04 +0200

    wip

--- src/app/(authorized)/admin/dashboard/events/event-dashboard/[id]/[tab]/players/Players.tsx
+++ src/app/(authorized)/admin/dashboard/events/event-dashboard/[id]/[tab]/players/Players.tsx
@@ -1,4 +1,5 @@
 import {
+    Box,
     Card,
     CardActions,
     CardHeader,
@@ -24,101 +25,13 @@ type Props = {
 };
 
 const Players = async ({ id }: Props) => {
-    // const event = await getEventById(id);
-
-    const poolPlayerInterests = await generatePoolPlayerInterestById('1AJUD3MiLuvnhf5AcXHD');
-    console.log('poolPlayerInterests', poolPlayerInterests);
-
-    // const dummyPlayerInrerestData: PlayerInterest = {
-    //     interestLevel: InterestLevel.VeryInterested,
-    //     poolEventId: '',
-    //     participantId: '',
-    //     firstName: 'Kari',
-    //     lastName: 'Nordmann',
-    //     isOver18: true,
-    //     ticketCategoryID: 0,
-    //     ticketCategory: 'Festivalpass Ungdom/student (13-25/30år) Early-bird',
-    //     conPlayers: [
-    //         {
-    //             id: undefined,
-    //             participantId: '',
-    //             firstName: '',
-    //             lastName: '',
-    //             interestLevel: InterestLevel.VeryInterested,
-    //             poolEventId: '',
-    //             poolEventTitle: 'Et gøyalt spill',
-    //             poolName: PoolName.fridayEvening,
-    //             isFirstChoice: true,
-    //             isGameMaster: false,
-    //             createdAt: '',
-    //             createdBy: '',
-    //             updateAt: '',
-    //             updatedBy: '',
-    //         },
-    //         {
-    //             id: undefined,
-    //             participantId: '',
-    //             firstName: '',
-    //             lastName: '',
-    //             interestLevel: InterestLevel.SomewhatInterested,
-    //             poolEventId: '',
-    //             poolEventTitle: '"Hva f**n skjedde!?!??" -- ghost/echo av John Harper',
-    //             poolName: PoolName.saturdayMorning,
-    //             isFirstChoice: false,
-    //             isGameMaster: true,
-    //             createdAt: '',
-    //             createdBy: '',
-    //             updateAt: '',
-    //             updatedBy: '',
-    //         },
-    //         {
-    //             id: undefined,
-    //             participantId: '',
-    //             firstName: '',
-    //             lastName: '',
-    //             interestLevel: InterestLevel.Interested,
-    //             poolEventId: '',
-    //             poolEventTitle: 'Random dnd event',
-    //             poolName: PoolName.saturdayMorning,
-    //             isFirstChoice: false,
-    //             isGameMaster: true,
-    //             createdAt: '',
-    //             createdBy: '',
-    //             updateAt: '',
-    //             updatedBy: '',
-    //         },
+    const event = await getEventById(id);
 
     return (
-        <Paper sx={{ padding: '1rem' }}>
+        <Box>
             <Typography variant="h1">Spillere:</Typography>
-            <Card sx={{ backgroundColor: 'rgb(55, 59, 87)' }}>
-                <CardHeader title="Prioriterg" />
-                <CardActions>
-                    <FormGroup sx={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
-                        <FormControlLabel control={<Switch />} label="Under 18" labelPlacement="start" />
-                        <FormControlLabel control={<Switch />} label="Over 18" labelPlacement="start" />
-                        <FormControlLabel control={<Switch />} label="Søndag Dagspass Barn" labelPlacement="start" />
-                    </FormGroup>
-                </CardActions>
-            </Card>
-            <Paper sx={{ backgroundColor: 'rgpa(0,0,0,0.1)', padding: '1rem' }}>
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="input-with-icon-adornment">Søk etter deltager</InputLabel>
-                    <Input
-                        id="input-with-icon-adornment"
-                        endAdornment={
-                            <InputAdornment position="start">
-                                <SearchIcon />
-                            </InputAdornment>
-                        }
-                    />
-                </FormControl>
-            </Paper>
-            <Typography variant="h2">Spillere:</Typography>
-            {poolPlayerInterests.map((poolPlayerInterest) => (
-                <PlayerInterestInfo poolName={PoolName.saturdayEvening} playerInterest={poolPlayerInterest} />
-            ))}
-        </Paper>
+            <Paper sx={{ padding: '1rem' }}></Paper>
+        </Box>
     );
 };
 
---
+++ src/app/(authorized)/admin/dashboard/events/event-dashboard/[id]/[tab]/players/components/PlayerManagement.tsx
@@ -0,0 +1,122 @@
+import {
+    Card,
+    CardActions,
+    CardHeader,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    Input,
+    InputAdornment,
+    InputLabel,
+    Paper,
+    Switch,
+    Typography,
+} from '@mui/material';
+import { PoolName } from '$lib/enums';
+import SearchIcon from '@mui/icons-material/Search';
+import { generatePoolPlayerInterestById } from './lib/actions';
+import PlayerInterestInfo from './PlayerInterestInfo';
+
+type Props = {
+    id: string;
+};
+
+const PlayerManagement = async ({ id }: Props) => {
+    // const event = await getEventById(id);
+
+    const poolPlayerInterests = await generatePoolPlayerInterestById(id);
+    console.log('poolPlayerInterests', poolPlayerInterests);
+
+    // const dummyPlayerInrerestData: PlayerInterest = {
+    //     interestLevel: InterestLevel.VeryInterested,
+    //     poolEventId: '',
+    //     participantId: '',
+    //     firstName: 'Kari',
+    //     lastName: 'Nordmann',
+    //     isOver18: true,
+    //     ticketCategoryID: 0,
+    //     ticketCategory: 'Festivalpass Ungdom/student (13-25/30år) Early-bird',
+    //     conPlayers: [
+    //         {
+    //             id: undefined,
+    //             participantId: '',
+    //             firstName: '',
+    //             lastName: '',
+    //             interestLevel: InterestLevel.VeryInterested,
+    //             poolEventId: '',
+    //             poolEventTitle: 'Et gøyalt spill',
+    //             poolName: PoolName.fridayEvening,
+    //             isFirstChoice: true,
+    //             isGameMaster: false,
+    //             createdAt: '',
+    //             createdBy: '',
+    //             updateAt: '',
+    //             updatedBy: '',
+    //         },
+    //         {
+    //             id: undefined,
+    //             participantId: '',
+    //             firstName: '',
+    //             lastName: '',
+    //             interestLevel: InterestLevel.SomewhatInterested,
+    //             poolEventId: '',
+    //             poolEventTitle: '"Hva f**n skjedde!?!??" -- ghost/echo av John Harper',
+    //             poolName: PoolName.saturdayMorning,
+    //             isFirstChoice: false,
+    //             isGameMaster: true,
+    //             createdAt: '',
+    //             createdBy: '',
+    //             updateAt: '',
+    //             updatedBy: '',
+    //         },
+    //         {
+    //             id: undefined,
+    //             participantId: '',
+    //             firstName: '',
+    //             lastName: '',
+    //             interestLevel: InterestLevel.Interested,
+    //             poolEventId: '',
+    //             poolEventTitle: 'Random dnd event',
+    //             poolName: PoolName.saturdayMorning,
+    //             isFirstChoice: false,
+    //             isGameMaster: true,
+    //             createdAt: '',
+    //             createdBy: '',
+    //             updateAt: '',
+    //             updatedBy: '',
+    //         },
+
+    return (
+        <Paper sx={{ padding: '1rem' }}>
+            <Card sx={{ backgroundColor: 'rgb(55, 59, 87)' }}>
+                <CardHeader title="Prioriterg" />
+                <CardActions>
+                    <FormGroup sx={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+                        <FormControlLabel control={<Switch />} label="Under 18" labelPlacement="start" />
+                        <FormControlLabel control={<Switch />} label="Over 18" labelPlacement="start" />
+                        <FormControlLabel control={<Switch />} label="Søndag Dagspass Barn" labelPlacement="start" />
+                    </FormGroup>
+                </CardActions>
+            </Card>
+            <Paper sx={{ backgroundColor: 'rgpa(0,0,0,0.1)', padding: '1rem' }}>
+                <FormControl variant="standard">
+                    <InputLabel htmlFor="input-with-icon-adornment">Søk etter deltager</InputLabel>
+                    <Input
+                        id="input-with-icon-adornment"
+                        endAdornment={
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+            </Paper>
+            <Typography variant="h2">Spillere:</Typography>
+            {poolPlayerInterests.map((poolPlayerInterest) => (
+                <PlayerInterestInfo poolName={PoolName.saturdayEvening} playerInterest={poolPlayerInterest} />
+            ))}
+        </Paper>
+    );
+};
+
+export default PlayerManagement;
