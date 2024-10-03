e552bedb31392aec7f778ae79c63495ddbbcc725
CoLdAs1cE <coldasice_t@hotmail.com>
2024-10-04 00:24:16 +0200

    Refactor and add loading state when waiting to create or update db

--- src/app/(authorized)/admin/dashboard/events/event-dashboard/[id]/[tab]/players/components/components/ui/AssignPlayerButtons.tsx
+++ src/app/(authorized)/admin/dashboard/events/event-dashboard/[id]/[tab]/players/components/components/ui/AssignPlayerButtons.tsx
@@ -1,40 +1,48 @@
 'use client';
-import { FormControlLabel, FormGroup, Switch } from '@mui/material';
+import { CircularProgress, FormControlLabel, FormGroup, Switch, Typography } from '@mui/material';
 import { useRouter } from 'next/navigation';
 import { assignPlayer } from '../lib/actions';
+import { useTransition } from 'react';
 
 type Props = {
-    poolPlayerId: string;
     participantId: string;
     poolEventId: string;
     isAssigned: boolean;
     isGameMaster: boolean;
+    poolPlayerId?: string;
 };
 
 const AssignPlayerButtons = ({ poolPlayerId, participantId, poolEventId, isAssigned, isGameMaster }: Props) => {
     const router = useRouter();
+    const [isPending, startTransition] = useTransition();
     const handleChanges = async (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, checked } = event.target;
-        const isAssigned = name === 'isAssigned' ? checked : false;
-        const isGameMaster = name === 'isGameMaster' ? checked : false;
-        await assignPlayer(poolPlayerId, participantId, poolEventId, isAssigned, isGameMaster);
-        console.log('event', event, 'name', name, 'checked', checked);
-        router.refresh();
+        startTransition(async () => {
+            const { checked } = event.target;
+            const name = event.target.name as 'isAssigned' | 'isGameMaster';
+            const isAssigned = name === 'isAssigned' ? checked : false;
+            const isGameMaster = name === 'isGameMaster' ? checked : false;
+            await assignPlayer(participantId, poolEventId, isAssigned, isGameMaster, poolPlayerId);
+            console.log('event', event, 'name', name, 'checked', checked);
+            router.refresh();
+        });
     };
 
-    return (
-        <FormGroup sx={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
-            <FormControlLabel
-                control={<Switch defaultChecked={isAssigned} name="isAssigned" onChange={handleChanges} />}
-                label="Tildel plass"
-                labelPlacement="start"
-            />
-            <FormControlLabel
-                control={<Switch defaultChecked={isGameMaster} name="isGameMaster" onChange={handleChanges} />}
-                label="GM"
-                labelPlacement="start"
-            />
-        </FormGroup>
-    );
+    return isPending ?
+            <FormGroup sx={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+                <FormControlLabel
+                    control={<Switch defaultChecked={isAssigned} name="isAssigned" onChange={handleChanges} />}
+                    label="Tildel plass"
+                    labelPlacement="start"
+                />
+                <FormControlLabel
+                    control={<Switch defaultChecked={isGameMaster} name="isGameMaster" onChange={handleChanges} />}
+                    label="GM"
+                    labelPlacement="start"
+                />
+            </FormGroup>
+        :   <>
+                <Typography sx={{ display: 'inline-block' }}>Oppdaterer listen med tildelte</Typography>{' '}
+                <CircularProgress size="1.5rem" />
+            </>;
 };
 export default AssignPlayerButtons;
