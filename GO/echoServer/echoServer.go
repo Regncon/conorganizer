284ce1d8407a095ca8f545a2f202a83cbe297597
CoLdAs1cE <coldasice_t@hotmail.com>
2024-10-29 02:09:52 +0100

    Add: tailwind

--- GO/echoServer/echoServer.go
+++ GO/echoServer/echoServer.go
@@ -19,17 +19,28 @@ func EchoServer() {
 	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(
 		rate.Limit(20),
 	)))
+	e.Static("/echo/dist", "echoServer/dist")
 
 	// This will initiate our template renderer
 	util.NewTemplateRenderer(e, "echoServer/public/*.html")
 	e.GET("/echo", func(e echo.Context) error {
 		c := echo.Context(e)
+
 		res := map[string]interface{}{
 			"Name":  "Wyndham",
 			"Phone": "8888888",
 			"Email": "skyscraper@gmail.com",
 		}
 		return c.Render(http.StatusOK, "index", res)
+	})
+
+	e.GET("/echo/get-info", func(c echo.Context) error {
+		res := map[string]interface{}{
+			"Name":  "Wyndham",
+			"Phone": "8888888",
+			"Email": "skyscraper@gmail.com",
+		}
+		return c.Render(http.StatusOK, "name_card", res)
 	})
 
 	e.Logger.Fatal(e.Start(":3000"))
---
+++ GO/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./echoServer/public/**/*.html'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
