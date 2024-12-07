6adfc26df087766722ed4bf5804be665b5c02821
Gerhard Just-Olsen <cinmay@pop-os.lan>
2024-12-07 11:00:25 +0100

    Basic project setup

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/a-h/templ-examples/hello-world
+module github.com/Regncon/conorganizer
 
 go 1.23.3
 
-require github.com/a-h/templ v0.2.793 // indirect
+require github.com/a-h/templ v0.2.793
--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/Regncon/conorganizer/pages/event"
+	"github.com/Regncon/conorganizer/pages/root"
 	"github.com/a-h/templ"
 	"net/http"
 )
 
 func main() {
-	component := hello("John")
 
-	http.Handle("/", templ.Handler(component))
+	http.Handle("/", templ.Handler(root.Page("Regncon 2025")))
+	http.Handle("/event", templ.Handler(event.Page("Regncon 2025")))
 
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
