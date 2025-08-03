945eac47cef8de8dc251e8e778b0aad988344a3d
kristoffer <kunkristoffer@gmail.com>
2025-08-03 19:19:33 +0200

    Added a type for accepted backup intervals instead of passing strings

---
+++ backup-service/models/backup.go
@@ -0,0 +1,10 @@
+package models
+
+type BackupInterval string
+
+const (
+	Hourly BackupInterval = "hourly"
+	Daily  BackupInterval = "daily"
+	Weekly BackupInterval = "weekly"
+	Yearly BackupInterval = "yearly"
+)
