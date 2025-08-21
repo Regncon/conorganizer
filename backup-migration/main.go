0386e0122372c6b5da58b423daa84e862692836f
Kristoffer <kunkristoffer@gmail.com>
2025-08-21 20:00:45 +0200

    adding todo notes for backup-migration

---
+++ backup-migration/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// Global variables: s3_id, s3_key, s3_url, s3_region, s3_bucket
+	// Changable variables: s3_path, db_path (where is db file located), upload_status
+
+	// Create window
+	// Sitt opp innput felts og upload fil
+	// Sitt opp log output felt
+
+	// Actions:
+	// Kjør sqlite validate på fil. Vis gjerne hvilke table og antall for manuell check
+	// Bruk s3 package for å laste opp, vis status
+
+	// Services: s3 client, s3 upload, s3 validate, sqlite read, validate
+}
