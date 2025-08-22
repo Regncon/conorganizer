66a46aae2bd036c822fd95952ad5c00e73d3c635
Kristoffer <kunkristoffer@gmail.com>
2025-08-22 16:19:02 +0200

    Creating backup service application (#142)
    * added volume bind during local development
    
    * added new models for database logger
    
    * moved config type into models
    
    * added default value to message in logger db sql
    
    * Added new logger functions for writing events to db
    
    * updated config imports in s3 functions
    
    * updated backupservice args in main
    
    * Added a service for sending discord webhooks on errors
    
    * Combined logger, database logger and discord webhook into a combined function responsible for handling error tracking
    
    * reverted type conversion since i fixed input types earlier
    
    * Added timezone tracking to application
    
    * Updated discord webhook output
    
    * removed unused variable from config

--- backup-service/models/backup.go
+++ backup-service/models/backup.go
@@ -1,11 +1,33 @@
 package models
 
+import (
+	"database/sql"
+	"log/slog"
+)
+
 type BackupInterval string
+type BackupStage string
 
 const (
-	Hourly   BackupInterval = "hourly"
-	Daily    BackupInterval = "daily"
-	Weekly   BackupInterval = "weekly"
-	Yearly   BackupInterval = "yearly"
-	Manually BackupInterval = "manually"
+	Hourly        BackupInterval = "hourly"
+	Daily         BackupInterval = "daily"
+	Weekly        BackupInterval = "weekly"
+	Yearly        BackupInterval = "yearly"
+	Manually      BackupInterval = "manually"
+	Initializing  BackupStage    = "starting"
+	Downloading   BackupStage    = "downloading"
+	Decompressing BackupStage    = "decompressing"
+	Moving        BackupStage    = "moving"
+	Finalizing    BackupStage    = "finalizing"
 )
+
+type BackupOutcome struct {
+	DB         *sql.DB
+	LogID      int64
+	Status     BackupLogStatus
+	Error      string
+	Stage      BackupStage
+	Interval   BackupInterval
+	Logger     *slog.Logger
+	WebhookURL string
+}
--- backup-service/services/backup.go
+++ backup-service/services/backup.go
@@ -2,11 +2,11 @@ package services
 
 import (
 	"context"
+	"database/sql"
 	"log/slog"
 	"os"
 	"path/filepath"
 
-	"github.com/Regncon/conorganizer/backup-service/config"
 	"github.com/Regncon/conorganizer/backup-service/models"
 	"github.com/Regncon/conorganizer/backup-service/utils"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -14,15 +14,17 @@ import (
 
 // BackupService handles scheduled backup operations and file rotation logic.
 type BackupService struct {
-	Config   config.Config
+	Config   models.Config
+	Db       *sql.DB
 	S3Client *s3.Client
 	Logger   *slog.Logger
 }
 
 // NewBackupService creates a new instance of BackupService with dependencies injected.
-func NewBackupService(cfg config.Config, s3Client *s3.Client, logger *slog.Logger) *BackupService {
+func NewBackupService(cfg models.Config, db *sql.DB, s3Client *s3.Client, logger *slog.Logger) *BackupService {
 	return &BackupService{
 		Config:   cfg,
+		Db:       db,
 		S3Client: s3Client,
 		Logger:   logger,
 	}
@@ -31,12 +33,32 @@ func NewBackupService(cfg config.Config, s3Client *s3.Client, logger *slog.Logge
 // run is the internal function that performs backup + rotation,
 // retention is how many files we need of that interval
 func (b *BackupService) run(ctx context.Context, interval models.BackupInterval, retention int) {
+	// Signal that backup service has started a job
 	b.Logger.Info("Scheduled backup job triggered", "interval", interval)
+
+	// Create db entry for logging
+	logID, err := NewLogBackup(b.Db, interval)
+	if err != nil {
+		b.Logger.Error("Failed to log backup", "err", err)
+		return
+	}
+
+	// Create status object for tracking
+	output := models.BackupOutcome{
+		DB:         b.Db,
+		LogID:      logID,
+		Interval:   interval,
+		Logger:     b.Logger,
+		WebhookURL: b.Config.DISCORD_WEBHOOK_URL,
+	}
 
 	// download snapshot
 	snapshotPath, err := DownloadLatestSnapshot(ctx, b.S3Client, b.Config.BUCKET_NAME, b.Config.DB_PREFIX)
 	if err != nil {
-		b.Logger.Error("Downloading snapshot failed", "err", err)
+		output.Status = models.Error
+		output.Stage = models.Downloading
+		output.Error = err.Error()
+		HandleBackupResult(output)
 		return
 	}
 	b.Logger.Info("Snapshot downloaded", "snapshot", snapshotPath)
@@ -100,4 +122,29 @@ func (b *BackupService) Yearly() {
 func (b *BackupService) Manual() {
 	ctx := context.Background()
 	b.run(ctx, models.Manually, 20)
+}
+
+func HandleBackupResult(outcome models.BackupOutcome) {
+	// Update log in DB
+	err := UpdateLogBackup(outcome.DB, models.BackupLogInput{
+		ID:      outcome.LogID,
+		Status:  outcome.Status,
+		Message: outcome.Error,
+	})
+	if err != nil {
+		outcome.Logger.Error("Failed to write to database", "stage", outcome.Stage, "error", err)
+	}
+
+	// Log result
+	if outcome.Status == models.Success {
+		outcome.Logger.Info("Backup stored successfully", "type", outcome.Interval)
+	} else {
+		outcome.Logger.Error("Backup failed", "stage", outcome.Stage, "type", outcome.Interval, "error", outcome.Error)
+	}
+
+	// Send discord notification
+	err = SendDiscordMessage(outcome)
+	if err != nil {
+		outcome.Logger.Error("Discord notification failed", "stage", outcome.Stage, "error", outcome.Error)
+	}
 }
