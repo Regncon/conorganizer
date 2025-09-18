95487e720ed8cc88676193f0fc4e1e0b9bcdcfdb
Kristoffer <kunkristoffer@gmail.com>
2025-09-18 19:25:31 +0200

    Migrating database to version 1.0.1 (#157)
    * Updated models with changes introduced to db schema
    
    * Updated form input for events age_group to use new model
    
    * updated litestream config
    
    * Adding dockerfile for local migration deployments to flyio

--- models/billettholder.go
+++ models/billettholder.go
@@ -2,24 +2,6 @@ package models
 
 import "time"
 
-/*
-CREATE TABLE IF NOT EXISTS billettholdere (
-
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	first_name TEXT NOT NULL,
-	last_name TEXT NOT NULL,
-	ticket_type TEXT NOT NULL,
-	is_over_18 BOOLEAN NOT NULL,
-	order_id INTEGER NOT NULL,
-	ticket_id INTEGER NOT NULL UNIQUE,
-	ticket_email TEXT NOT NULL,
-	order_email TEXT NOT NULL,
-	ticket_category_id TEXT NOT NULL,
-	inserted_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (ticket_type) REFERENCES ticket_types(name)
-
-);
-*/
 type Billettholder struct {
 	ID               int       `json:"id"`
 	FirstName        string    `json:"first_name"`
