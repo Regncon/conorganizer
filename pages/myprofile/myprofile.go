72427616522f76cf79fe479a319b54b2d1174038
Kristoffer <kunkristoffer@gmail.com>
2025-09-30 22:28:01 +0200

    reverting changes to original profile page

--- pages/myprofile/myprofile.go
+++ pages/myprofile/myprofile.go
@@ -18,12 +18,10 @@ func SetupMyProfileRoute(router chi.Router, store sessions.Store, ns *embeddedna
 	router.Route("/my-profile", func(ticketRouter chi.Router) {
 		ticketRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			var ctx = r.Context()
-			var user = userctx.GetUserRequestInfo(ctx)
-
 			layouts.Base(
 				"Min profil",
 				userctx.GetUserRequestInfo(ctx),
-				myProfile(user, db, logger),
+				myProfile(),
 			).Render(ctx, w)
 
 		})
