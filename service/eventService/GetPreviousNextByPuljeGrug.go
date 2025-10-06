e1ff252294362a7fb396f49f3c3b81f1c01abe36
Gerhard Just-Olsen <cinmay05@gmail.com>
2025-10-06 22:03:57 +0200

    It works

--- service/eventService/GetPreviousNextByPuljeGrug.go
+++ service/eventService/GetPreviousNextByPuljeGrug.go
@@ -12,6 +12,57 @@ import (
 	"github.com/Regncon/conorganizer/service/eventimage"
 )
 
+func getPreviousPuljeId(currentPuljeId models.Pulje) models.Pulje {
+	if currentPuljeId == models.PuljeLordagMorgen {
+		return models.PuljeFredagKveld
+	}
+	if currentPuljeId == models.PuljeLordagKveld {
+		return models.PuljeLordagMorgen
+	}
+	if currentPuljeId == models.PuljeSondagMorgen {
+		return models.PuljeLordagKveld
+	}
+	// If it's the first pulje, return itself
+	return models.PuljeFredagKveld
+}
+
+func getNextPuljeId(currentPuljeId models.Pulje) models.Pulje {
+	if currentPuljeId == models.PuljeFredagKveld {
+		return models.PuljeLordagMorgen
+	}
+	if currentPuljeId == models.PuljeLordagMorgen {
+		return models.PuljeLordagKveld
+	}
+	if currentPuljeId == models.PuljeLordagKveld {
+		return models.PuljeSondagMorgen
+	}
+	// If it's the last pulje, return itself
+	return models.PuljeSondagMorgen
+}
+
+func getLastEventInPriviousPulje(eventsByPulje root.EventsByPulje, currentPuljeId models.Pulje) *models.EventCardModel {
+	var puljeId = getPreviousPuljeId(currentPuljeId)
+
+	if ebp, ok := eventsByPulje[puljeId]; ok {
+		if len(ebp.Events) > 0 {
+			return &ebp.Events[len(ebp.Events)-1]
+		}
+	}
+	emptyEvent := models.EventCardModel{}
+	return &emptyEvent
+}
+
+func getFirstEventInPulje(eventsByPulje root.EventsByPulje, currentPuljeId models.Pulje) *models.EventCardModel {
+	var puljeId = getNextPuljeId(currentPuljeId)
+	if ebp, ok := eventsByPulje[puljeId]; ok {
+		if len(ebp.Events) > 0 {
+			return &ebp.Events[0]
+		}
+	}
+	emptyEvent := models.EventCardModel{}
+	return &emptyEvent
+}
+
 func GetPreviousNextByPuljeSimple(
 	ctx context.Context,
 	eventsByPulje root.EventsByPulje,
@@ -55,14 +106,27 @@ func GetPreviousNextByPuljeSimple(
 				if event.Id == currentID {
 					fmt.Println("Found current event at index:", i)
 					if i > 0 {
+						fmt.Println("Getting previous event in the same pulje")
 						previousId = ebp.Events[i-1].Id
 						previousTitle = ebp.Events[i-1].Title
 						previousPuljeID = currentPunjeId
+					} else if ebp.Pulje.ID != models.PuljeFredagKveld {
+						lastEvent := getLastEventInPriviousPulje(eventsByPulje, currentPunjeId)
+						previousId = lastEvent.Id
+						previousTitle = lastEvent.Title
+						previousPuljeID = getPreviousPuljeId(currentPunjeId)
 					}
+
 					if i < len(ebp.Events)-1 {
 						nextId = ebp.Events[i+1].Id
 						nextTitl = ebp.Events[i+1].Title
 						nextPuljeID = currentPunjeId
+					} else if ebp.Pulje.ID != models.PuljeSondagMorgen {
+						fmt.Println("Getting first event from next pulje:", nextPuljeID)
+						firstEvent := getFirstEventInPulje(eventsByPulje, currentPunjeId)
+						nextId = firstEvent.Id
+						nextTitl = firstEvent.Title
+						nextPuljeID = getNextPuljeId(currentPunjeId)
 					}
 				}
 			}
@@ -77,10 +141,17 @@ func GetPreviousNextByPuljeSimple(
 	if nextId != "" {
 		nextBanner = eventimage.GetEventImageUrl(nextId, "banner", eventImageDir)
 	}
+	previousUrl := ""
+	if previousId != "" {
+		previousUrl = fmt.Sprintf("/event/%s?pulje=%s", previousId, previousPuljeID)
+	}
+	nextUrl := ""
+	if nextId != "" {
+		nextUrl = fmt.Sprintf("/event/%s?pulje=%s", nextId, nextPuljeID)
+	}
+	fmt.Println("previousId:", previousId, "previousTitle:", previousTitle, "nextId:", nextId, "nextTitle:", nextTitl)
 
 	fmt.Println("previous puljeID:", previousPuljeID)
-	var previousUrl = fmt.Sprintf("%s?pulje=%s", previousId, previousPuljeID)
-	var nextUrl = fmt.Sprintf("%s?pulje=%s", nextId, nextPuljeID)
 	var result = components.PreviousNext{
 		PreviousUrl:      previousUrl,
 		PreviousTitle:    previousTitle,
