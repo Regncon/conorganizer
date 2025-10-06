e043c65b98724d44cbc23aa81fe5033b1acfae87
Kristoffer <kunkristoffer@gmail.com>
2025-10-06 23:42:52 +0200

    Fixed uniqueness in associate tickets with billettholder

--- service/checkIn/assign.go
+++ service/checkIn/assign.go
@@ -10,6 +10,12 @@ import (
 	"github.com/Regncon/conorganizer/models"
 )
 
+// uniquePair is used for getting unique values when comparing tickets and billettholder
+type uniquePair struct {
+	TicketID int
+	Ticket   CheckInTicket
+}
+
 // AssociateTicketsWithEmail takes a list of tickets and matches with email supplied, returns matches
 func AssociateTicketsWithEmail(tickets []CheckInTicket, email string) ([]CheckInTicket, error) {
 	var result []CheckInTicket
@@ -30,28 +36,86 @@ func AssociateTicketsWithEmail(tickets []CheckInTicket, email string) ([]CheckIn
 	return result, nil
 }
 
-// todo AssociateBillettholderWithEmail should be called and then feed inn result to billettholder table
-// todo etter funksjon er called og data skal føres inn i db. Ikke opprett nye billettholdere som eksisterer fra før, og ikke koblinger (billettholder_users)
-
-func AssociateTicketsWithBillettholder(tickets []CheckInTicket, email string) error {
+// AssociateTicketsWithBillettholder is responsible for finding tickets registered on an email and
+// inserting new unique tickets into billettholder
+func AssociateTicketsWithBillettholder(tickets []CheckInTicket, email string, db *sql.DB, logger *slog.Logger) error {
 	// Filtrer tickets til de som er registrert på user email
-	_, err := AssociateTicketsWithEmail(tickets, email)
+	associatedTickets, err := AssociateTicketsWithEmail(tickets, email)
 	if err != nil {
-		return err
+		// No associated tickets found, quitting early
+		fmt.Printf("Found no tickets associated with %s, quitting early\n", email)
+		return nil
 	}
+	fmt.Printf("Found %d/%d tickets associated with %s\n", len(associatedTickets), len(tickets), email)
+
+	// get list of ticket ids to exclude when quering billettholder
 
 	// get existing billetterholdere registered to user email
+	var billettholdereIDs []models.Billettholder
+	rows, err := db.Query(`
+        SELECT DISTINCT b.ticket_id
+        FROM billettholder_emails e
+        JOIN billettholdere b ON b.id = e.billettholder_id
+        WHERE e.email = ? COLLATE NOCASE;
+    `, email)
+	if err != nil {
+		return fmt.Errorf("unable to query billettholder: %v", err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var result models.Billettholder
+		err := rows.Scan(&result.TicketID)
+		if err != nil {
+			return fmt.Errorf("unable to scan billettholder: %v", err)
+		}
+		billettholdereIDs = append(billettholdereIDs, result)
+	}
+	fmt.Printf("Found %d existing billettholdere with email: %s\n", len(billettholdereIDs), email)
 
-	// Create new array of unique non-existing tickets
+	// Create new map of unique non-existing tickets
+	var uniqueNewTicketsMap = map[uniquePair]struct{}{}
+	for _, associatedTicket := range associatedTickets {
+		var billettholderExists = false
 
-	// Enter new array into billettholdere
+		for _, billetholderID := range billettholdereIDs {
+			if billetholderID.TicketID == associatedTicket.ID {
+				billettholderExists = true
+				break
+			}
+		}
 
-	// call AssociateUserWithBillettholder
+		if !billettholderExists {
+			uniqueNewTicketsMap[uniquePair{TicketID: associatedTicket.ID, Ticket: associatedTicket}] = struct{}{}
+		}
+	}
 
+	// Convert unique map back to array
+	var uniqueNewTickets []CheckInTicket
+	for pair := range uniqueNewTicketsMap {
+		uniqueNewTickets = append(uniqueNewTickets, pair.Ticket)
+	}
+
+	// No new unique tickets, quitting early
+	if len(uniqueNewTickets) == 0 {
+		fmt.Println("Found no new unique tickets to add, quitting early")
+		return nil
+	}
+
+	// Enter new array into billettholdere ... newTicket needs to be unique?
+	for _, ticket := range uniqueNewTickets {
+		err = converTicketIdToNewBillettholder(ticket.ID, uniqueNewTickets, db, logger)
+		fmt.Printf("Adding Ticket: %+v \n", ticket)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("Added %d new billettholdere from %d tickets\n", len(uniqueNewTickets), len(tickets))
 	return nil
 }
 
 // AssociateUserWithBillettholder uses userID string from users table to match billettholders
+// and combine ids to billettholder_users for later lookup
 func AssociateUserWithBillettholder(userID string, db *sql.DB, logger *slog.Logger) error {
 	logger.Info("Associating userID with billettholder", "userID", userID)
 
