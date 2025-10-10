7a0a6e0471b2090c334515baa565489d040d1d21
Ørjan Polden <oerjanbpolden@gmail.com>
2025-10-10 14:34:02 +0200

    229 | Update to display Pulje(r) in event details (#261)
    * do some layout updates on the event details section
    
    * make event-details section of page be its own internal container
    
    * increase spacing between event detail sets
    
    * Show pulje on event page
    
    * adjust styling
    
    * fix wrong size comparison
    
    * fix misplaced details on event page
    
    * remove merge sugar stuck in the code
    
    * fix spacings below event
    
    * narrower margin below event heading on frontpage on mobile
    
    * fix layouting of tickets pane on profile page
    
    * adjust event-card height
    
    ---------
    
    Co-authored-by: Gerhard Just-Olsen <cinmay05@gmail.com>

--- service/eventService/event_helpers.go
+++ service/eventService/event_helpers.go
@@ -64,6 +64,66 @@ func GetEventById(eventID string, db *sql.DB, logger *slog.Logger) (*models.Even
 	return &event, nil
 }
 
+func GetPujerForEvent(
+	eventID string,
+	db *sql.DB,
+	logger *slog.Logger) ([]models.PuljeRow, error) {
+	/*
+			   CREATE TABLE
+			       event_puljer (
+			           event_id TEXT NOT NULL,
+			           pulje_id TEXT NOT NULL,
+			           is_active BOOLEAN NOT NULL DEFAULT TRUE,
+			           is_published BOOLEAN NOT NULL DEFAULT FALSE,
+			           room TEXT DEFAULT '',
+			           PRIMARY KEY (event_id, pulje_id),
+			           FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE,
+			           FOREIGN KEY (pulje_id) REFERENCES puljer (id) ON UPDATE CASCADE
+			       );
+		CREATE TABLE
+		    puljer (
+		        id TEXT NOT NULL PRIMARY KEY,
+		        name TEXT NOT NULL,
+		        start_time DATE NOT NULL,
+		        end_time DATE NOT NULL
+		    );
+
+		type PuljeRow struct {
+			ID        Pulje     `json:"id"`
+			Name      string    `json:"name"`
+			StartTime time.Time `json:"start_time"`
+			EndTime   time.Time `json:"end_time"`
+		}
+	*/
+	query := `SELECT p.id, p.name, p.start_time, p.end_time
+            FROM puljer p
+            JOIN event_puljer ep ON p.id = ep.pulje_id
+            WHERE ep.event_id = ? AND ep.is_active = TRUE AND ep.is_published = TRUE
+            ORDER BY p.start_time ASC
+            `
+
+	rows, err := db.Query(query, eventID)
+	if err != nil {
+		logger.Error("Error querying puljer for event", slog.String("eventID", eventID), slog.String("error", err.Error()))
+		return nil, err
+	}
+	defer rows.Close()
+	var puljer []models.PuljeRow
+	for rows.Next() {
+		var pulje models.PuljeRow
+		if err := rows.Scan(&pulje.ID, &pulje.Name, &pulje.StartTime, &pulje.EndTime); err != nil {
+			logger.Error("Error scanning pulje row", slog.String("eventID", eventID), slog.String("error", err.Error()))
+			return nil, err
+		}
+		puljer = append(puljer, pulje)
+	}
+	if err := rows.Err(); err != nil {
+		logger.Error("Error iterating over pulje rows", slog.String("eventID", eventID), slog.String("error", err.Error()))
+		return nil, err
+	}
+	return puljer, nil
+}
+
 /*
 func GetEventByID(id string, db *sql.DB) (*models.Event, error) {
 	query := `
