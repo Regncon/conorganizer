c884f8aff0c120e1c15679d2ac5bf4668785fc3b
CoLdAs1cE <coldasice_t@hotmail.com>
2026-02-01 17:46:18 +0100

    updated naming and sql for view

--- migrations/20260201161247_views.sql
+++ migrations/20260201161247_views.sql
@@ -1,12 +1,19 @@
 -- +goose Up
 CREATE VIEW IF NOT EXISTS
-    get_billettholder_id AS
+    v_get_user_billettholder AS
 SELECT
-    u.*,
-    bu.*
+    u.id AS user_db_id,
+    u.user_id AS user_id,
+    u.email AS user_email,
+    u.is_admin AS user_is_admin,
+    u.inserted_time AS user_inserted_time,
+    bu.billettholder_id AS billettholder_id,
+    bu.user_id AS billettholder_user_db_id,
+    bu.inserted_time AS billettholder_user_inserted_time
 FROM
     billettholdere_users AS bu
     LEFT JOIN users AS u ON u.id = bu.user_id;
 
 -- +goose Down
-DROP VIEW IF EXISTS get_billettholder_id;
+DROP VIEW IF EXISTS v_get_user_billettholder;
+
