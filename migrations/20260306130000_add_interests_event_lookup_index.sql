446b8a9bac327c790c16aff2be9811ddd4243098
CoLdAs1cE <coldasice_t@hotmail.com>
2026-03-08 19:36:00 +0100

    Moved migration to rebuilding interests

--- migrations/20260306130000_add_interests_event_lookup_index.sql
+++
@@ -1,6 +0,0 @@
--- +goose Up
-CREATE INDEX IF NOT EXISTS `idx_interests_event_lookup`
-ON `interests` (event_id, pulje_id, interest_level, billettholder_id);
-
--- +goose Down
-DROP INDEX IF EXISTS `idx_interests_event_lookup`;
