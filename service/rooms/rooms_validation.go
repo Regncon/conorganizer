85b7e1f709b8b66dc44fadf583c670af064bd3dd
Kristoffer <kunkristoffer@gmail.com>
2026-05-24 17:16:56 +0200

    Forgot to add function brackets to empty validation functions

--- service/rooms/rooms_validation.go
+++ service/rooms/rooms_validation.go
@@ -3,10 +3,10 @@ package rooms
 import "database/sql"
 
 // ValidateRooms validates that all entries in `room` is valid
-func ValidateRooms(db *sql.DB)
+func ValidateRooms(db *sql.DB) {}
 
 // ValidateRoomsByPulje is used for validating that a snapshot of rooms, based on a pulje, is valid (eg. assigned vs max concurrent events per pulje)
-func ValidateRoomsByPulje(db *sql.DB, puljeID string)
+func ValidateRoomsByPulje(db *sql.DB, puljeID string) {}
 
 // ValidateDisabledRoomsCascade Validates that room disabled status has cascaded and no orphans exist in `relation_event_puljer`
-func ValidateDisabledRoomsCascade(db *sql.DB)
+func ValidateDisabledRoomsCascade(db *sql.DB) {}
