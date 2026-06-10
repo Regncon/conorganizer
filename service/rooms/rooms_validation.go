cb7110360c1f7c1086c98a1eb05e61031931546a
Kristoffer <kunkristoffer@gmail.com>
2026-06-10 12:46:56 +0200

    Extracted validation logic for room signals into room_validation in the room service

--- service/rooms/rooms_validation.go
+++ service/rooms/rooms_validation.go
@@ -1,9 +1,31 @@
 package rooms
 
-import "database/sql"
+import (
+	"database/sql"
+	"strconv"
+	"strings"
 
-// ValidateRooms validates that all entries in `room` is valid
-func ValidateRooms(db *sql.DB) {}
+	"github.com/Regncon/conorganizer/models"
+)
+
+// ValidateRooms validates that required entries in `room` is valid and returns all encutered errors
+func ValidateRooms(room models.Room) models.RoomFormErrors {
+	errors := models.RoomFormErrors{}
+
+	if strings.TrimSpace(room.RoomNumber) == "" {
+		errors.RoomNumber = "Romnummer er påkrevd"
+	}
+
+	if !strings.HasPrefix(room.RoomNumber, strconv.Itoa(room.Floor)) {
+		errors.RoomNumber = "Romnummer må starte med etasje som første tall"
+	}
+
+	if room.MaxConcurrentGames < 1 {
+		errors.MaxConcurrentGames = "Maks samtidige spill må være minst 1"
+	}
+
+	return errors
+}
 
 // ValidateRoomsByPulje is used for validating that a snapshot of rooms, based on a pulje, is valid (eg. assigned vs max concurrent events per pulje)
 func ValidateRoomsByPulje(db *sql.DB, puljeID string) {}
