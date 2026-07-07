5dd74008332e2bdebcd5c4b1c1d6398976afa1c6
cirashrii <cirashrii@gmail.com>
2026-07-07 12:17:07 +0200

    removed a couple comments

--- service/eventService/event_helpers.go
+++ service/eventService/event_helpers.go
@@ -14,12 +14,10 @@ import (
 )
 
 func MdToHTML(md []byte) []byte {
-	// create markdown parser with extensions
 	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
 	p := parser.NewWithExtensions(extensions)
 	doc := p.Parse(md)
 
-	// create HTML renderer with extensions
 	htmlFlags := html.CommonFlags | html.HrefTargetBlank
 	opts := html.RendererOptions{Flags: htmlFlags}
 	renderer := html.NewRenderer(opts)
